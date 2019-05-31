2c627ee36c057833d2fec6b6d24f535dd581cd40
yukihane <yukihane.feather@gmail.com>
2019-06-01 07:19:05 +0900

    auto-generated files

---
+++ so48348/src/main/java/com/github/yukihane/so48348/So48348Application.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.so48348;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So48348Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So48348Application.class, args);
+	}
+
+}
---
+++ so48348/src/test/java/com/github/yukihane/so48348/So48348ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.so48348;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class So48348ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
