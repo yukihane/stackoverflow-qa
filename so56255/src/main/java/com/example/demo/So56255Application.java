f34e20ca2b3489922a3fa1884efb401a41cf2705
yukihane <yukihane.feather@gmail.com>
2019-07-01 13:16:09 +0900

    auto-generated files

---
+++ so56255/src/main/java/com/example/demo/So56255Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So56255Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So56255Application.class, args);
+	}
+
+}
