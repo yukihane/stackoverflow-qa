3abda411e55688fe71f88181b2d18288c85373de
yukihane <yukihane.feather@gmail.com>
2019-07-17 13:41:17 +0900

    auto-generated files

---
+++ so56679/src/main/java/com/example/so56679/So56679Application.java
@@ -0,0 +1,13 @@
+package com.example.so56679;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So56679Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So56679Application.class, args);
+	}
+
+}
