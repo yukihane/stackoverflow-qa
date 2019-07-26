d5e4bc57328b452a2103afb3c4a5fbf319235abe
yukihane <yukihane.feather@gmail.com>
2019-07-26 11:11:48 +0900

    auto-generated files

---
+++ so56891/src/main/java/com/github/yukihane/so56891/So56891Application.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.so56891;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So56891Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So56891Application.class, args);
+	}
+
+}
