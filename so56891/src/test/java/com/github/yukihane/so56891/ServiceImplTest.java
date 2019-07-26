49bb106b0c3224fa7665795954819fadae1b8f8b
yukihane <yukihane.feather@gmail.com>
2019-07-26 11:29:08 +0900

    質問者の意図した挙動になるようにテストコードを修正

--- so56891/src/test/java/com/github/yukihane/so56891/ServiceImplTest.java
+++ so56891/src/test/java/com/github/yukihane/so56891/ServiceImplTest.java
@@ -7,18 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 @RunWith(SpringRunner.class)
-@TestExecutionListeners({
-    DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class
-})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ServiceImplTest {
     @Mock
     private Repository rep;
