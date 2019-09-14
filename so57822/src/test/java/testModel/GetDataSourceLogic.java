5ca05326c2bd32e5b4856f9639affbbdd7f525ea
yukihane <yukihane.feather@gmail.com>
2019-09-14 16:20:02 +0900

    修正コード

--- so57822/src/test/java/testModel/GetDataSourceLogic.java
+++ so57822/src/test/java/testModel/GetDataSourceLogic.java
@@ -8,15 +8,15 @@ import org.h2.jdbcx.JdbcDataSource;
 public class GetDataSourceLogic {
     public JdbcDataSource getH2DataSource() throws IOException {
 
-        final Properties props = new Properties();
-        final JdbcDataSource dataSource = new JdbcDataSource();
-        try (InputStream in = ClassLoader.class.getResourceAsStream("/jdbc/JdbcDataSource_H2.properties")) {
+        try (InputStream in = GetDataSourceLogic.class.getResourceAsStream("/jdbc/JdbcDataSource_H2.properties")) {
+            final Properties props = new Properties();
             props.load(in);
+            final JdbcDataSource dataSource = new JdbcDataSource();
             dataSource.setURL(props.getProperty("JDBC_URL"));
             dataSource.setUser(props.getProperty("USER"));
             dataSource.setPassword(props.getProperty("PASSWORD"));
+            return dataSource;
         }
-        return dataSource;
 
     }
 }
