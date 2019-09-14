55f6dc944ecf81731694848743dfa809ea924859
yukihane <yukihane.feather@gmail.com>
2019-09-14 12:38:42 +0900

    質問文のオリジナルソース

---
+++ so57857/src/main/java/com/example/so57857/MyController.java
@@ -0,0 +1,34 @@
+package com.example.so57857;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+    @RequestMapping(value = "/profile/save", method = RequestMethod.GET)
+    public String S161formList(Model model) {
+
+        model.addAttribute("S100601formList", new S161formList());
+        return "S161.html";
+    }
+
+    @RequestMapping(value = "/profile/save", method = RequestMethod.POST)
+    public String form(@ModelAttribute 161formList 161formList1, Model model1) {
+
+        System.out.println(id);
+System.out.println(password);
+        return "S161.html";
+    }
+
+
+    public class S161formList {
+
+    private int id;
+    private int password;
+
+各setter getter
+}
+}
