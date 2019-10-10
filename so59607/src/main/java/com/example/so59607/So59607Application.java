d30e1ad6a95ad169cfff341ed76b12740c3e2c37
yukihane <yukihane.feather@gmail.com>
2019-10-10 13:17:25 +0900

    auto-generated files

---
+++ so59607/src/main/java/com/example/so59607/So59607Application.java
@@ -0,0 +1,13 @@
+package com.example.so59607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So59607Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(So59607Application.class, args);
+    }
+
+}
---
+++ so59607/src/test/java/com/example/so59607/So59607ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.so59607;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class So59607ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
