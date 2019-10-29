29c01d959517d1af38692e2e33b712f3a312d522
yukihane <yukihane.feather@gmail.com>
2019-10-29 15:49:01 +0900

    auto-generated files

---
+++ so60029/src/main/java/com/github/yukihane/oropattern/OroPatternApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.oropattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OroPatternApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(OroPatternApplication.class, args);
+    }
+
+}
