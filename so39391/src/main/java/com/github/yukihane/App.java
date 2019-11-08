c1847c5799630e13635fd4d471b33d875d170e74
yukihane <yukihane.feather@gmail.com>
2019-11-08 23:12:45 +0900

    サンプルプログラム

--- so39391/src/main/java/com/github/yukihane/App.java
+++ so39391/src/main/java/com/github/yukihane/App.java
@@ -1,11 +1,36 @@
 package com.github.yukihane;
 
-/**
- * Hello world!
- */
-public class App {
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends JFrame {
+
+    private static final long serialVersionUID = -3034008389766810149L;
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public App() {
+
+        super("HELLO WORLD");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JLabel label = new JLabel("Hello World");
+        getContentPane().add(label);
+        setPreferredSize(new Dimension(150, 100));
+
+        pack();
+    }
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+
+        LOG.info("Hello, executable JAR!");
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            final App app = new App();
+            app.setVisible(true);
+        });
     }
 }
