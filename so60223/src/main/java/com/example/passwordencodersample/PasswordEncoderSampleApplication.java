2577df06d6f4bb95adc94896dc870daab54ab496
yukihane <yukihane.feather@gmail.com>
2019-11-19 09:24:20 +0900

    init

---
+++ so60223/src/main/java/com/example/passwordencodersample/PasswordEncoderSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.passwordencodersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncoderSampleApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PasswordEncoderSampleApplication.class, args);
+    }
+
+}
