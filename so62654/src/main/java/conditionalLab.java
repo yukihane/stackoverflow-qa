d60628cb96753277309dd9044afb287c4e43735e
yukihane <yukihane.feather@gmail.com>
2020-02-01 09:18:54 +0900

    回答に書かれたコード

--- so62654/src/main/java/conditionalLab.java
+++ so62654/src/main/java/conditionalLab.java
@@ -8,11 +8,11 @@ public class conditionalLab extends JPanel {
     ImageIcon picture2;
     ImageIcon picture3;
     ImageIcon picture4;
-    int favorite;
+    int jobs;
 
     public conditionalLab() {
         // Your custom initialization code here
-        JOptionPane.showConfirmDialog(null, "Are you Student or Military?", "Laie Palms Cinemas",
+        jobs = JOptionPane.showConfirmDialog(null, "Are you Student or Military?", "Laie Palms Cinemas",
                 JOptionPane.YES_NO_OPTION);
 
         final String mike = JOptionPane.showInputDialog("Please enter your age.");
@@ -27,27 +27,38 @@ public class conditionalLab extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         // Your custom rendering code here
-        if ((favorite == JOptionPane.YES_OPTION) && (age >= 11) && (age <= 60)) {
+
+        if ((jobs == JOptionPane.YES_OPTION) && (age >= 11) && (age <= 60)) {
             g.drawString("That will be $8.00, please", 10, 30);
             picture3.paintIcon(null, g, 10, 50);
 
-        } else if ((favorite == JOptionPane.NO_OPTION) && (age >= 0) && (age <= 2)) {
+        }
+
+        if ((jobs == JOptionPane.NO_OPTION) && (age >= 0) && (age <= 2)) {
             g.drawString("You get free!", 10, 10);
             picture1.paintIcon(null, g, 10, 50);
 
-        } else if ((favorite == JOptionPane.NO_OPTION) && (age >= 3) && (age <= 11)) {
+        }
+
+        if ((jobs == JOptionPane.NO_OPTION) && (age >= 3) && (age <= 11)) {
             g.drawString("You owe $7.00!", 10, 10);
             picture2.paintIcon(null, g, 10, 50);
 
-        } else if ((favorite == JOptionPane.NO_OPTION) && (age >= 12) && (age <= 59)) {
+        }
+
+        if ((jobs == JOptionPane.NO_OPTION) && (age >= 12) && (age <= 59)) {
             g.drawString("You owe $10.00!", 10, 10);
             picture3.paintIcon(null, g, 10, 50);
 
-        } else if ((favorite == JOptionPane.NO_OPTION) && (age >= 60) && (age <= 119)) {
+        }
+
+        if ((jobs == JOptionPane.NO_OPTION) && (age >= 60) && (age <= 119)) {
             g.drawString("That will be $7.00, please", 10, 10);
             picture4.paintIcon(null, g, 10, 50);
 
-        } else if ((favorite == JOptionPane.NO_OPTION) && (age >= 120) && (age <= -1)) {
+        }
+
+        else {
             g.drawString("Are you sure you typed that correctly?", 10, 10);
 
         }
