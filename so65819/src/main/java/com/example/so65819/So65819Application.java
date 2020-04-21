67d9b88fa04bd835d093fbce49360c6c94fed425
yukihane <yukihane.feather@gmail.com>
2020-04-21 21:31:50 +0900

    auto-generated files

---
+++ so65819/src/main/java/com/example/so65819/So65819Application.java
@@ -0,0 +1,13 @@
+package com.example.so65819;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So65819Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So65819Application.class, args);
+	}
+
+}
---
+++ so65819/src/test/java/com/example/so65819/So65819ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so65819;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So65819ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
