7f6cd2be7ccdaaaa56445283f1fc0867a9322e23
yukihane <yukihane.feather@gmail.com>
2020-04-24 11:45:04 +0900

    auto-generated files

---
+++ so65859/src/main/java/com/github/yukihane/so/messagespropertiesexample/MessagesPropertiesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.so.messagespropertiesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesPropertiesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesPropertiesExampleApplication.class, args);
+	}
+
+}
---
+++ so65859/src/test/java/com/github/yukihane/so/messagespropertiesexample/MessagesPropertiesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.so.messagespropertiesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesPropertiesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
