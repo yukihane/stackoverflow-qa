d4f039fdb50962af98893c649ab2dada9572b90c
yukihane <yukihane.feather@gmail.com>
2020-04-28 14:04:45 +0900

    型が間違っていてコンパイルエラーになっている箇所を一旦コメントアウト

--- so65386/src/App.tsx
+++ so65386/src/App.tsx
@@ -7,26 +7,27 @@ interface IState {
   texxt: boolean;
 }
 
-const index:FC = () => {
+const index: FC = () => {
   const [state, setState] = useState<IState>();
 
- const changeState = (key: IState) => {
-    console.log(key)
+  const changeState = (key: IState) => {
+    console.log(key);
     // setStateでうまくstateを変更できない
-    setState({ ...state, ...{ key: !key }});
+    // setState({ ...state, ...{ key: !key }});
   };
 
- <Switch
-  checked={numberState}
-  onChange={(e) => changeState({ numberState: e.target.checked })}
- />
- <Switch
-  checked={textState}
-  onChange={(e) => changeState({ textState: e.target.checked })}
- />
- <Switch
-  checked={symbolState}
-  onChange={(e) => changeState({ symbolState: e.target.checked })}
- />
-
+  //  <Switch
+  //   checked={numberState}
+  //   onChange={(e) => changeState({ numberState: e.target.checked })}
+  //  />
+  //  <Switch
+  //   checked={textState}
+  //   onChange={(e) => changeState({ textState: e.target.checked })}
+  //  />
+  //  <Switch
+  //   checked={symbolState}
+  //   onChange={(e) => changeState({ symbolState: e.target.checked })}
+  //  />
+  return <div></div>;
+};
 export default index;
