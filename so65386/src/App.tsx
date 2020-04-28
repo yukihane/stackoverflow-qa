34b2434acdad1bf717a92ec6f682ec6f25030fad
yukihane <yukihane.feather@gmail.com>
2020-04-28 14:03:42 +0900

    質問コード

--- so65386/src/App.tsx
+++ so65386/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
+import Switch from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  numbers: boolean;
+  symbol: boolean;
+  texxt: boolean;
 }
 
-export default App;
+const index:FC = () => {
+  const [state, setState] = useState<IState>();
+
+ const changeState = (key: IState) => {
+    console.log(key)
+    // setStateでうまくstateを変更できない
+    setState({ ...state, ...{ key: !key }});
+  };
+
+ <Switch
+  checked={numberState}
+  onChange={(e) => changeState({ numberState: e.target.checked })}
+ />
+ <Switch
+  checked={textState}
+  onChange={(e) => changeState({ textState: e.target.checked })}
+ />
+ <Switch
+  checked={symbolState}
+  onChange={(e) => changeState({ symbolState: e.target.checked })}
+ />
+
+export default index;
