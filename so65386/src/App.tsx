a9f38125598b2641a1823c4d40bea27d215439e8
yukihane <yukihane.feather@gmail.com>
2020-04-28 15:14:16 +0900

    回答コード

--- so65386/src/App.tsx
+++ so65386/src/App.tsx
@@ -1,33 +1,41 @@
 import React, { FC, useState } from "react";
-import Switch from "@material-ui/core";
+import { Switch } from "@material-ui/core";
 
 interface IState {
-  numbers: boolean;
-  symbol: boolean;
-  texxt: boolean;
+  numberState: boolean;
+  symbolState: boolean;
+  textState: boolean;
 }
 
-const index: FC = () => {
-  const [state, setState] = useState<IState>();
+const initIState = {
+  numberState: false,
+  symbolState: false,
+  textState: false,
+};
 
-  const changeState = (key: IState) => {
+const Index: FC = () => {
+  const [state, setState] = useState<IState>(initIState);
+
+  const changeState = (key: Partial<IState>) => {
     console.log(key);
-    // setStateでうまくstateを変更できない
-    // setState({ ...state, ...{ key: !key }});
+    setState({ ...state, ...key });
   };
 
-  //  <Switch
-  //   checked={numberState}
-  //   onChange={(e) => changeState({ numberState: e.target.checked })}
-  //  />
-  //  <Switch
-  //   checked={textState}
-  //   onChange={(e) => changeState({ textState: e.target.checked })}
-  //  />
-  //  <Switch
-  //   checked={symbolState}
-  //   onChange={(e) => changeState({ symbolState: e.target.checked })}
-  //  />
-  return <div></div>;
+  return (
+    <>
+      <Switch
+        checked={state.numberState}
+        onChange={(e) => changeState({ numberState: e.target.checked })}
+      />
+      <Switch
+        checked={state.textState}
+        onChange={(e) => changeState({ textState: e.target.checked })}
+      />
+      <Switch
+        checked={state.symbolState}
+        onChange={(e) => changeState({ symbolState: e.target.checked })}
+      />
+    </>
+  );
 };
-export default index;
+export default Index;
