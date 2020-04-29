7c4d9ee4927c17939fd0df95b226d90a42f67d2b
yukihane <yukihane.feather@gmail.com>
2020-04-29 18:58:38 +0900

    コンパイルエラー解消

--- so66100/src/main/java/com/github/yukihane/so/ZTestCalculation.java
+++ so66100/src/main/java/com/github/yukihane/so/ZTestCalculation.java
@@ -1,24 +1,24 @@
 package com.github.yukihane.so;
 
-import org.rosuda.JRI.REXP;
-import org.rosuda.JRI.Rengine;
+//import org.rosuda.JRI.REXP;
+//import org.rosuda.JRI.Rengine;
 import javax.swing.*;
 import java.awt.BorderLayout;
 
-public class ZTestCalculation
+public class ZTestCalculation extends JFrame
 {
      public static void main (String[] args)
      {
-        //This is a method to create graph and save it as png file
-        Rengine engine = new Rengine(new String[]{"--no-save"}, false, null);
-        engine.eval("png('normal.png', 640, 480)");
-        engine.eval("plot(dnorm, -4, 4)");
-        engine.eval("xvals <- seq(qnorm(10), 4, length=100)");
-        engine.eval("dvals <- dnorm(xvals)");
-        engine.eval("polygon(c(xvals,rev(xvals)),c(rep(0,100),rev(dvals)),col=\"gray\")");
-        engine.eval("dev.off()");
-        engine.end();
-
+//        //This is a method to create graph and save it as png file
+//        Rengine engine = new Rengine(new String[]{"--no-save"}, false, null);
+//        engine.eval("png('normal.png', 640, 480)");
+//        engine.eval("plot(dnorm, -4, 4)");
+//        engine.eval("xvals <- seq(qnorm(10), 4, length=100)");
+//        engine.eval("dvals <- dnorm(xvals)");
+//        engine.eval("polygon(c(xvals,rev(xvals)),c(rep(0,100),rev(dvals)),col=\"gray\")");
+//        engine.eval("dev.off()");
+//        engine.end();
+//
 //This is a method to stick png file to JFrame 
         ZTestCalculation frame = new ZTestCalculation();
 
@@ -32,6 +32,6 @@ public class ZTestCalculation
         JLabel label = new JLabel(icon);
         label.setIcon(icon); 
         p.add(label);
-        getContentPane().add(p, BorderLayout.CENTER);
+        frame.getContentPane().add(p, BorderLayout.CENTER);
     }
 }
