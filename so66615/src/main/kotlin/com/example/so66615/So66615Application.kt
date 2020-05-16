95dadd5be1553f762a210a3099ca6a49e9e4fe4b
yukihane <yukihane.feather@gmail.com>
2020-05-16 11:23:16 +0900

    spring initializr で 2.2.7 ベースのテンプレート作成

---
+++ so66615/src/main/kotlin/com/example/so66615/So66615Application.kt
@@ -0,0 +1,11 @@
+package com.example.so66615
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class So66615Application
+
+fun main(args: Array<String>) {
+	runApplication<So66615Application>(*args)
+}
---
+++ so66615/src/test/kotlin/com/example/so66615/So66615ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.so66615
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class So66615ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
