7b7e67f619d13c1008a1865ac31c36a9077006e3
yukihane <yukihane.feather@gmail.com>
2020-05-16 09:50:58 +0900

    実行コード

--- so66633/src/main/java/com/github/yukihane/so/App.java
+++ so66633/src/main/java/com/github/yukihane/so/App.java
@@ -6,6 +6,11 @@ package com.github.yukihane.so;
 public class App {
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        int x=100;
+        Bus bus = new Bus(x);
+        Person person = new Adult("myname", 300);
+        bus.getOn(person);
+        Person person2 = new Adult("myname2", 300);
+        bus.getOn(person2);
     }
 }
