bbfd1bb658ee74d12faee40457402a33c1313bd2
yukihane <yukihane.feather@gmail.com>
2020-05-22 03:28:35 +0900

    回答コード

---
+++ so66783/src/main/kotlin/App.kt
@@ -0,0 +1,22 @@
+interface IHoge {
+    fun test()
+}
+
+class A : IHoge {
+    override fun test() {}
+}
+
+class C : IHoge {
+    @Deprecated(message = "利用不能メソッド", level = DeprecationLevel.ERROR)
+    override fun test() {
+        println("C")
+    }
+}
+
+fun main(args: Array<String>) {
+    val c = C();
+    c.test(); // コンパイルエラー
+
+    val i: IHoge = C();
+    i.test(); // コンパイルエラーにならない
+}
