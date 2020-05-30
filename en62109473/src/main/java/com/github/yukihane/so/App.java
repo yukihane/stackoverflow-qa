3543034b29a23dd59d733dddad9e3d0b37d403ef
yukihane <yukihane.feather@gmail.com>
2020-05-31 08:14:22 +0900

    answer: https://stackoverflow.com/a/62109676/4506703

--- en62109473/src/main/java/com/github/yukihane/so/App.java
+++ en62109473/src/main/java/com/github/yukihane/so/App.java
@@ -10,7 +10,10 @@ import java.net.URL;
 public class App {
 
     public static void main(final String[] args) {
-        final URL url = Object.class.getResource("/org/mozillatranslator/resource/mt_icon_small.gif");
+        final URL url = App.class.getResource("/org/mozillatranslator/resource/mt_icon_small.gif");
+        //        final URL url = Thread.currentThread()
+        //            .getContextClassLoader()
+        //            .getResource("org/mozillatranslator/resource/mt_icon_small.gif");
         final Toolkit tk = Toolkit.getDefaultToolkit();
 
         final Image img = tk.getImage(url); // Raises runtime exception
