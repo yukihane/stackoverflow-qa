ec8ef0119c7f897818e9a421db95b6dd50375fc2
yukihane <yukihane.feather@gmail.com>
2020-05-31 08:53:51 +0900

    init

---
+++ en62109821/src/main/java/com/example/en62109821/En62109821Application.java
@@ -0,0 +1,13 @@
+package com.example.en62109821;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class En62109821Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(En62109821Application.class, args);
+	}
+
+}
---
+++ en62109821/src/test/java/com/example/en62109821/En62109821ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.en62109821;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class En62109821ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
