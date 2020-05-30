e62fe1a6e3d66a5eb285cb145e132a1e061cd89d
yukihane <yukihane.feather@gmail.com>
2020-05-31 07:53:58 +0900

    質問コード

--- en62109473/src/main/java/com/github/yukihane/so/App.java
+++ en62109473/src/main/java/com/github/yukihane/so/App.java
@@ -1,11 +1,18 @@
 package com.github.yukihane.so;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        final URL url = Object.class.getResource("/org/mozillatranslator/resource/mt_icon_small.gif");
+        final Toolkit tk = Toolkit.getDefaultToolkit();
+
+        final Image img = tk.getImage(url); // Raises runtime exception
     }
 }
