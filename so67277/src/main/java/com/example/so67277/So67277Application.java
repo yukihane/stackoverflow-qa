93a036347504628d39f0a2a032e883db9027c346
yukihane <yukihane.feather@gmail.com>
2020-06-05 03:22:46 +0900

    jdbc driver

---
+++ so67277/src/main/java/com/example/so67277/So67277Application.java
@@ -0,0 +1,13 @@
+package com.example.so67277;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So67277Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So67277Application.class, args);
+	}
+
+}
---
+++ so67277/src/test/java/com/example/so67277/So67277ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so67277;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So67277ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
