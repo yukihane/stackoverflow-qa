59bfdb02d5963210d9df372e622dc2da7b8c215c
yukihane <yukihane.feather@gmail.com>
2020-06-05 17:44:55 +0900

    コンパイルエラー解消

--- so67315/src/main/java/java8/Main.java
+++ so67315/src/main/java/java8/Main.java
@@ -5,11 +5,12 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         // TODO 自動生成されたメソッド・スタブ
         System.out.println(Thread.currentThread().getName() + ": main() started.");
         //Googleの株価
@@ -36,7 +37,7 @@ public class Main {
      * @param ticker 銘柄のID
      * @return 株価情報テーブル
      */
-    private static Stream downloadStockInfo(final String ticker) {
+    private static Stream<String> downloadStockInfo(final String ticker) {
         try {
             URL url = new URL("https://ichart.finance.yahoo.com/table.csv?s=" + ticker);
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -52,9 +53,9 @@ public class Main {
      * @param stockInfo 株価情報テーブル
      * @return 株価情報テーブルから抽出した終値
      */
-    private static Stream extractClosePrices(Stream stockInfo) {
-        Stream closePricesStr = stockInfo.skip(1).map(x -> x.split(",")[4]);
-        Stream closePrices = closePricesStr.map(BigDecimal::new);
+    private static Stream<BigDecimal> extractClosePrices(Stream<String> stockInfo) {
+        Stream<String> closePricesStr = stockInfo.skip(1).map(x -> x.split(",")[4]);
+        Stream<BigDecimal> closePrices = closePricesStr.map(BigDecimal::new);
         System.out.println(Thread.currentThread().getName() + ": getClosePrice() executed.");
         return closePrices;
     }
@@ -63,7 +64,7 @@ public class Main {
      * 終値の平均値を計算し出力する
      * @param closePrices 終値の数年分のリスト
      */
-    private static void printAveragePrice(Stream closePrices){
+    private static void printAveragePrice(Stream<BigDecimal> closePrices){
         Double average = closePrices.mapToDouble(BigDecimal::doubleValue).average().getAsDouble();
         System.out.println(Thread.currentThread().getName() + ": printAveragePrice() executed. avg " + average + ".");
     }
