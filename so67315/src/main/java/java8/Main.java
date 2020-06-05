51b35fe597f2eb938f6b934068832f30eef33967
yukihane <yukihane.feather@gmail.com>
2020-06-05 17:34:12 +0900

    質問コード

---
+++ so67315/src/main/java/java8/Main.java
@@ -0,0 +1,71 @@
+package java8;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println(Thread.currentThread().getName() + ": main() started.");
+        //Googleの株価
+        CompletableFuture cf1 = CompletableFuture.supplyAsync(() -> downloadStockInfo("GOOG"))
+                .thenApply(Main::extractClosePrices)
+                .thenAccept(Main::printAveragePrice);
+        //Amazonの株価
+        CompletableFuture cf2 = CompletableFuture.supplyAsync(() -> downloadStockInfo("AMZN"))
+                .thenApply(Main::extractClosePrices)
+                .thenAccept(Main::printAveragePrice);
+        //Appleの株価
+        CompletableFuture cf3 = CompletableFuture.supplyAsync(() -> downloadStockInfo("AAPL"))
+                .thenApply(Main::extractClosePrices)
+                .thenAccept(Main::printAveragePrice);
+        System.out.println(Thread.currentThread().getName() + ": main() running.");
+        cf1.get();
+        cf2.get();
+        cf3.get();
+        System.out.println(Thread.currentThread().getName() + ": main() end.");
+    }
+
+    /**
+     * YahooファイナンスのURLから指定した銘柄の数年分の株価情報を取得する
+     * @param ticker 銘柄のID
+     * @return 株価情報テーブル
+     */
+    private static Stream downloadStockInfo(final String ticker) {
+        try {
+            URL url = new URL("https://ichart.finance.yahoo.com/table.csv?s=" + ticker);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            System.out.println(Thread.currentThread().getName() + ": downloadStockInfo() executed. ticker " + ticker);
+            return reader.lines();
+        } catch(Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 株価情報テーブルから終値を抽出する
+     * @param stockInfo 株価情報テーブル
+     * @return 株価情報テーブルから抽出した終値
+     */
+    private static Stream extractClosePrices(Stream stockInfo) {
+        Stream closePricesStr = stockInfo.skip(1).map(x -> x.split(",")[4]);
+        Stream closePrices = closePricesStr.map(BigDecimal::new);
+        System.out.println(Thread.currentThread().getName() + ": getClosePrice() executed.");
+        return closePrices;
+    }
+
+    /**
+     * 終値の平均値を計算し出力する
+     * @param closePrices 終値の数年分のリスト
+     */
+    private static void printAveragePrice(Stream closePrices){
+        Double average = closePrices.mapToDouble(BigDecimal::doubleValue).average().getAsDouble();
+        System.out.println(Thread.currentThread().getName() + ": printAveragePrice() executed. avg " + average + ".");
+    }
+
+}
