6692dcf6eef12d59dc816d5ea5ce4070bdb19d81
yukihane <yukihane.feather@gmail.com>
2020-06-10 08:55:07 +0900

    このサンプルはフィールドインジェクションに統一することにする

--- en32244745/src/main/java/com/github/yukihane/springsecurityauthexample/config/MyWebSecurityConfigurerAdapter.java
+++ en32244745/src/main/java/com/github/yukihane/springsecurityauthexample/config/MyWebSecurityConfigurerAdapter.java
@@ -1,16 +1,16 @@
 package com.github.yukihane.springsecurityauthexample.config;
 
 import com.github.yukihane.springsecurityauthexample.security.MyAuthenticationProvider;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    private final MyAuthenticationProvider authProvider;
+    @Autowired
+    private MyAuthenticationProvider authProvider;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
