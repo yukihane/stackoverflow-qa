429bfa397929ff8c3fec34bf3122946fb5c669a3
yukihane <yukihane.feather@gmail.com>
2020-06-15 19:21:59 +0900

    init

---
+++ so67679/src/test/java/com/example/so67679/So67679ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so67679;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So67679ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
