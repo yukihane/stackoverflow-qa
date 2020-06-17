9dc5120473fda7be0b93be1a7795ac830bb22ab3
yukihane <yukihane.feather@gmail.com>
2020-06-17 16:41:53 +0900

    init

---
+++ so67716/src/test/java/com/example/so67716/So67716ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so67716;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So67716ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
