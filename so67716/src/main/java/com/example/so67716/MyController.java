43d398f464973ba8a356d418e00d24493802f647
yukihane <yukihane.feather@gmail.com>
2020-06-17 17:46:23 +0900

    https://github.com/spring-projects/spring-security/tree/5.3.3.RELEASE/samples/boot/oauth2login からのコピペ

---
+++ so67716/src/main/java/com/example/so67716/MyController.java
@@ -0,0 +1,23 @@
+package com.example.so67716;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String index(final Model model,
+        @RegisteredOAuth2AuthorizedClient final OAuth2AuthorizedClient authorizedClient,
+        @AuthenticationPrincipal final OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+        return "index";
+    }
+}
