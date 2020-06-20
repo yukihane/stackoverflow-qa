501084e870d0361bb92241bb98c8e65b14aab950
yukihane <yukihane.feather@gmail.com>
2020-06-21 08:48:25 +0900

    answer

--- en62490937/src/main/java/com/example/en62490937/RemoveParticipantFromConferenceRequest.java
+++ en62490937/src/main/java/com/example/en62490937/RemoveParticipantFromConferenceRequest.java
@@ -1,9 +1,11 @@
 package com.example.en62490937;
 
+import javax.validation.Valid;
 import lombok.Data;
 
 @Data
 public class RemoveParticipantFromConferenceRequest {
+    @Valid
     private Participant participant;
     private Conference conference;
 }
