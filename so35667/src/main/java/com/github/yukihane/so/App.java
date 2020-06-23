7bad58c7b2547415c78d6fd976aebaef37a508e3
yukihane <yukihane.feather@gmail.com>
2020-06-23 11:41:35 +0900

    質問コード

--- so35667/src/main/java/com/github/yukihane/so/App.java
+++ so35667/src/main/java/com/github/yukihane/so/App.java
@@ -1,11 +1,39 @@
 package com.github.yukihane.so;
 
-/**
- * Hello world!
- */
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.Data;
+
 public class App {
 
+    @Data
+    public static class car {
+        private long carId;
+
+        public long carId() {
+            return getCarId();
+        }
+    }
+
+    private long carId;
+
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        new App().method();
+    }
+
+    public void method() {
+        final List<car> carList = List.of();
+
+        // boolean isExist = false;  //エラーになる
+        final AtomicBoolean isExist = new AtomicBoolean(false);
+        for (final car c : carList) {
+            if (c.carId() == this.carId) {
+                isExist.set(true);
+            }
+        }
+
+        carList.stream()
+            .filter(c -> c.getCarId() == this.carId)
+            .forEach(i -> isExist.set(true));
     }
 }
