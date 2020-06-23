a7fee4c3bbc913d3cde85cd024246edfd22d0afd
yukihane <yukihane.feather@gmail.com>
2020-06-23 15:44:49 +0900

    init

---
+++ en62526262/src/main/java/com/example/en62526262/En62526262Application.java
@@ -0,0 +1,13 @@
+package com.example.en62526262;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class En62526262Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(En62526262Application.class, args);
+	}
+
+}
