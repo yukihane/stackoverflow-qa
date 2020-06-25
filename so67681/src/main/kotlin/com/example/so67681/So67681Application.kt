0d7a376b950100ff9b098b489d2590de42683844
yukihane <yukihane.feather@gmail.com>
2020-06-25 14:17:32 +0900

    init

---
+++ so67681/src/main/kotlin/com/example/so67681/So67681Application.kt
@@ -0,0 +1,11 @@
+package com.example.so67681
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class So67681Application
+
+fun main(args: Array<String>) {
+	runApplication<So67681Application>(*args)
+}
---
+++ so67681/src/test/kotlin/com/example/so67681/So67681ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.so67681
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class So67681ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
