15ee0bed573ef22de5675bdcb46e304aafa7c3f4
yukihane <yukihane.feather@gmail.com>
2020-06-26 23:37:00 +0900

    answer

--- en62594348/src/main/java/com/example/en62594348/En62594348Application.java
+++ en62594348/src/main/java/com/example/en62594348/En62594348Application.java
@@ -1,13 +1,26 @@
 package com.example.en62594348;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class En62594348Application {
+//@SpringBootConfiguration
+@ComponentScan
+//@SpringBootApplication
+@RequiredArgsConstructor
+public class En62594348Application implements CommandLineRunner {
+
+    private final MyClass myClass;
 
     public static void main(final String[] args) {
         SpringApplication.run(En62594348Application.class, args);
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        System.out.println(myClass.hello());
+        System.exit(0);
     }
 
 }
---
+++ en62594348/src/main/java/com/example/en62594348/MyClass.java
@@ -0,0 +1,11 @@
+package com.example.en62594348;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyClass {
+
+    public String hello() {
+        return "hello";
+    }
+}
