cdd8e4f29d743b1f35231f9b4a05c2e53b674771
yukihane <yukihane.feather@gmail.com>
2020-06-27 20:37:25 +0900

    質問コード

---
+++ en62607140/src/main/java/com/example/en62607140/VaultEngineApplication.java
@@ -0,0 +1,20 @@
+package com.example.en62607140;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class VaultEngineApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(VaultEngineApplication.class, args);
+    }
+
+    @GetMapping("/h")
+    public String greetingForm() {
+        return "index.html";
+    }
+}
