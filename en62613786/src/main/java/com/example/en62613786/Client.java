18cba69b91a8e6ec2792563c64aee67a1b8f922e
yukihane <yukihane.feather@gmail.com>
2020-06-28 05:47:18 +0900

    answer: https://stackoverflow.com/a/62615389/4506703

--- en62613786/src/main/java/com/example/en62613786/Client.java
+++ en62613786/src/main/java/com/example/en62613786/Client.java
@@ -1,10 +1,13 @@
 package com.example.en62613786;
 
 import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -15,7 +18,10 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private ArrayList<Order> orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     private Float cash;
 
     //I have getters and setters
--- en62613786/src/main/java/com/example/en62613786/Order.java
+++ en62613786/src/main/java/com/example/en62613786/Order.java
@@ -1,9 +1,23 @@
 package com.example.en62613786;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Data;
 
+@Entity
 @Data
+@Table(name = "ORDERS")
 public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Client client;
+
     private int price;
     private String name;
 }
