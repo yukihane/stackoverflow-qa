f72520a10ebeef0c29e9a2ff3d7c998d6fd80fc2
yukihane <yukihane.feather@gmail.com>
2020-06-27 20:34:44 +0900

    init

---
+++ en62607140/src/test/java/com/example/en62607140/En62607140ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.en62607140;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class En62607140ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
