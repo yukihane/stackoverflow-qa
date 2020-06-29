8d69d27dfba93cd3752bd354b2330016123bc371
yukihane <yukihane.feather@gmail.com>
2020-06-30 00:04:25 +0900

    answer: https://stackoverflow.com/a/62640972/4506703

--- en62635356/src/main/java/com/example/en62635356/CondClass.java
+++ en62635356/src/main/java/com/example/en62635356/CondClass.java
@@ -2,8 +2,10 @@ package com.example.en62635356;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Service;
 
+@ConditionalOnClass(MyConf.class)
 @ConditionalOnBean(MyConf.class)
 @Service
 @RequiredArgsConstructor
