252a5732759b45ec552a2600b2b360dfc3453671
yukihane <yukihane.feather@gmail.com>
2020-06-30 22:13:54 +0900

    再現コード

---
+++ en62657087/src/main/java/com/example/en62657087/MyController.java
@@ -0,0 +1,16 @@
+package com.example.en62657087;
+
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+
+    @GetMapping("/")
+    public Date date() {
+        return new Date();
+    }
+}
