efdf05d45b26791ecdf1ff3e04ad1b43ac4b118d
yukihane <yukihane.feather@gmail.com>
2020-07-02 11:42:42 +0900

    一旦コミット

--- en62674216/src/main/java/com/example/en62674216/En62674216Application.java
+++ en62674216/src/main/java/com/example/en62674216/En62674216Application.java
@@ -1,12 +1,12 @@
 package com.example.en62674216;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan
 public class En62674216Application {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(En62674216Application.class, args);
 	}
 
---
+++ en62674216/src/main/java/com/example/en62674216/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.en62674216;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    @Bean
+    ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory(8080);
+    }
+}
