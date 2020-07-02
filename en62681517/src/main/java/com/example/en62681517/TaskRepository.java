0954b890f32e2639667906b0629db7e9a1f43db6
yukihane <yukihane.feather@gmail.com>
2020-07-02 10:56:43 +0900

    Spring Data REST を使ってみる: https://stackoverflow.com/a/62687793/4506703

---
+++ en62681517/src/main/java/com/example/en62681517/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.example.en62681517;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
--- en62681517/src/main/resources/data.sql
+++ en62681517/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into tasks (id, title, description) values (1, 'the-title', null)
+insert into comments(id, task_id, comment) values (2, 1, 'the-comment')
