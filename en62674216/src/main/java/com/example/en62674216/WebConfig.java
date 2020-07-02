330b9613fda1edb9495a80a45d44cd2afadd71ae
yukihane <yukihane.feather@gmail.com>
2020-07-02 11:54:25 +0900

    minimal webmvc

--- en62674216/src/main/java/com/example/en62674216/WebConfig.java
+++ en62674216/src/main/java/com/example/en62674216/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -13,5 +14,10 @@ public class WebConfig {
     @Bean
     ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory(8080);
+    }
+
+    @Bean
+    DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 }
