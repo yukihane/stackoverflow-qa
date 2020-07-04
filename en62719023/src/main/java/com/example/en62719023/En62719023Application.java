35847b41b4b9bafdb985a32a75f06e7f8173bbc5
yukihane <yukihane.feather@gmail.com>
2020-07-04 22:45:48 +0900

    int

---
+++ en62719023/src/main/java/com/example/en62719023/En62719023Application.java
@@ -0,0 +1,13 @@
+package com.example.en62719023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class En62719023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(En62719023Application.class, args);
+	}
+
+}
--- en62727660/src/main/java/com/example/en62727660/En62727660Application.java
+++ en62727660/src/main/java/com/example/en62727660/En62727660Application.java
@@ -2,11 +2,12 @@ package com.example.en62727660;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication //(exclude = { ErrorMvcAutoConfiguration.class })
 public class En62727660Application {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(En62727660Application.class, args);
 	}
 
