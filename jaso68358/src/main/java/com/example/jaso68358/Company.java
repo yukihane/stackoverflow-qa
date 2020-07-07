3f71fe138b3d622cfa3623eb881ff5d1ccf808ab
yukihane <yukihane.feather@gmail.com>
2020-07-07 18:20:42 +0900

    サンプルコード

---
+++ jaso68358/src/main/java/com/example/jaso68358/Company.java
@@ -0,0 +1,20 @@
+package com.example.jaso68358;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import org.dom4j.Branch;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "company")
+    private List<Branch> branches;
+}
---
+++ jaso68358/src/main/java/com/example/jaso68358/Employee.java
@@ -0,0 +1,13 @@
+package com.example.jaso68358;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
