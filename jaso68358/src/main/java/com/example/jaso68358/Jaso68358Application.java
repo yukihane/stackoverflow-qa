1a55ac916115a666106c664398372f90d5593bae
yukihane <yukihane.feather@gmail.com>
2020-07-07 15:58:17 +0900

    init

---
+++ jaso68358/src/main/java/com/example/jaso68358/Jaso68358Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso68358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso68358Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso68358Application.class, args);
+	}
+
+}
---
+++ jaso68358/src/test/java/com/example/jaso68358/Jaso68358ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso68358;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso68358ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
