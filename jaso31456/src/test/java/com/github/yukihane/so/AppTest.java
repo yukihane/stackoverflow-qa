b4d7ead5ac024b5ed841eeb26c76b125d1f9c8a1
yukihane <yukihane.feather@gmail.com>
2020-07-11 04:03:58 +0900

    実行コード

--- jaso31456/src/test/java/com/github/yukihane/so/AppTest.java
+++ jaso31456/src/test/java/com/github/yukihane/so/AppTest.java
@@ -1,19 +1,31 @@
 package com.github.yukihane.so;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
+    public static final String TEXT = "[1,2]";
+
     @Test
-    public void testApp() {
-        assertThat(true).isEqualTo(false);
+    public void parseWithJackson() throws JsonMappingException, JsonProcessingException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final int[] result = mapper.readValue(TEXT, int[].class);
+
+        assertThat(result).isEqualTo(new int[] { 1, 2 });
+    }
+
+    @Test
+    public void slitAsStream() {
+        final Pattern p = Pattern.compile("\\D+");
+        final int[] result = p.splitAsStream(TEXT)
+            .filter(str -> !str.isEmpty())
+            .mapToInt(Integer::parseInt).toArray();
+        assertThat(result).isEqualTo(new int[] { 1, 2 });
     }
 }
