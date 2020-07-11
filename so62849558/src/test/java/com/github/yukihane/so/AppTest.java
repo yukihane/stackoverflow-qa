6614b259a5fea201df4764b277ce8df2371059ab
yukihane <yukihane.feather@gmail.com>
2020-07-11 22:47:09 +0900

    answer: https://stackoverflow.com/a/62849971/4506703

--- so62849558/src/test/java/com/github/yukihane/so/AppTest.java
+++ so62849558/src/test/java/com/github/yukihane/so/AppTest.java
@@ -1,19 +1,34 @@
 package com.github.yukihane.so;
 
-import static org.assertj.core.api.Assertions.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
-import org.junit.jupiter.api.*;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
+    private static String json;
+
+    @BeforeAll
+    public static void setUp() throws IOException {
+        try (final BufferedReader r = new BufferedReader(
+            new InputStreamReader(AppTest.class.getClassLoader().getResourceAsStream("testdata.txt")))) {
+            json = r.lines().collect(Collectors.joining("\n")).toString();
+        }
+    }
+
     @Test
-    public void testApp() {
-        assertThat(true).isEqualTo(false);
+    public void testApp() throws JSONException {
+        final String text = "{" + json;
+        final JSONObject obj = new JSONObject(text);
+        final String api = obj.getString("api");
+        final JSONObject object2 = new JSONObject(api);
+        final String first = object2.getJSONArray("predictions").getJSONObject(0).getString("teams");
+
+        System.out.println(first);
     }
 }
