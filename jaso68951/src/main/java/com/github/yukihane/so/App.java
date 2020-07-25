0e20e708a0ba7277aed0d951eb0b946960b71274
yukihane <yukihane.feather@gmail.com>
2020-07-25 22:35:15 +0900

    answer

--- jaso68951/src/main/java/com/github/yukihane/so/App.java
+++ jaso68951/src/main/java/com/github/yukihane/so/App.java
@@ -1,9 +1,15 @@
 package com.github.yukihane.so;
 
+import org.eclipse.swt.internal.win32.OS;
+import org.eclipse.swt.internal.win32.TCHAR;
+
 public class App {
 
     public static void main(final String[] args) {
-        final int hWnd = OS.FindWindow(null, new TCHAR(OS.CP_INSTALLED, "(無題) - 秀丸",
+        final long hWnd = OS.FindWindow(null, new TCHAR(OS.CP_INSTALLED,
+            "無題 - メモ帳",
             true));
+
+        System.out.println(hWnd);
     }
 }
