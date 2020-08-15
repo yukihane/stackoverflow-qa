b5febe873786b610aab1d049a4dd6cb92e6a9dec
yukihane <yukihane.feather@gmail.com>
2020-08-15 19:08:32 +0900

    回答例

--- jaso52956/src/main/java/com/github/yukihane/so/CreateMosaic.java
+++ jaso52956/src/main/java/com/github/yukihane/so/CreateMosaic.java
@@ -18,7 +18,7 @@ public class CreateMosaic {
         w=jsonObject.getAsJsonObject().get("mosaic_size_w").getAsInt();
         h=jsonObject.getAsJsonObject().get("mosaic_size_h").getAsInt();
         int imgNum[]=new int[w*h];
-        for (int i=2;i<jsonObject.size();i++){
+        for (int i = 0; i < jsonObject.size() - 3; i++) {
             imgNum[i]=jsonObject.getAsJsonObject().get(String.valueOf(i)).getAsInt();
         }
         Log.d("size",String.valueOf(size));
