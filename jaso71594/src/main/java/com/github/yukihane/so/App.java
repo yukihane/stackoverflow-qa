e4d618854ade7d633873c7aaa062e583b56ca57d
yukihane <yukihane.feather@gmail.com>
2020-10-29 20:43:37 +0900

    回答の確認コード

--- jaso71594/src/main/java/com/github/yukihane/so/App.java
+++ jaso71594/src/main/java/com/github/yukihane/so/App.java
@@ -1,11 +1,17 @@
 package com.github.yukihane.so;
 
-/**
- * Hello world!
- */
+import java.nio.charset.Charset;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 public class App {
 
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
+    public static void main(final String[] args) throws DecoderException {
+
+        final char[] bytes = Hex.encodeHex("ぁ".getBytes(Charset.forName("Shift_JIS")));
+        System.out.println(bytes); // 829f
+
+        final byte[] str = Hex.decodeHex("82F1");
+        System.out.println(new String(str, Charset.forName("Shift_JIS")));
     }
 }
