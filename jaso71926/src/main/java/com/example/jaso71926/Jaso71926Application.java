db7c5017276831f199646f54a93f6a441451c863
yukihane <yukihane.feather@gmail.com>
2020-11-14 00:20:13 +0900

    init

---
+++ jaso71926/src/main/java/com/example/jaso71926/Jaso71926Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso71926;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso71926Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso71926Application.class, args);
+	}
+
+}
---
+++ jaso71926/src/test/java/com/example/jaso71926/Jaso71926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso71926;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso71926ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
