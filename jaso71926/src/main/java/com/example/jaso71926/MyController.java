a84b3b2e2ec2d5ccf7948dc2c722d2fea63e5220
yukihane <yukihane.feather@gmail.com>
2020-11-14 04:48:44 +0900

    kari

---
+++ jaso71926/src/main/java/com/example/jaso71926/MyController.java
@@ -0,0 +1,49 @@
+package com.example.jaso71926;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+//    // デフォルトマッピング
+//    @GetMapping
+//    @RequestMapping(value = "/**")
+//    @ResponseBody
+//    String another(HttpServletRequest request) {
+//        return "<HTML>Forbidden</HTML>";
+//    }
+
+    // ターゲットリクエストマッピング
+    @GetMapping
+    @RequestMapping(value = "/*")
+    void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        String uri = request.getRequestURI();
+        if (uri.equals("/")) {
+
+            // 実際にはこの地点で認証などの共通処理を実施
+
+            response.getWriter().write(" <HTML>\n" +
+                "            <HEAD>\n" +
+                "                <TITLE>Sample-Page</TITLE>\n" +
+                "            <HEAD>\n" +
+                "            <BODY>\n" +
+                "                <H1>Sample-Page</H1>\n" +
+                "                <script src=\"./js/sample.js\"></script>\n" +
+                "            </BODY>\n" +
+                "        </HTML>");
+
+        } else {
+            response.getWriter().write("<HTML>Forbidden</HTML>");
+        }
+    }
+}
