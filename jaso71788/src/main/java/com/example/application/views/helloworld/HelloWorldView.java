b7b51fa0c672b56783fe123458bb2342a976219a
yukihane <yukihane.feather@gmail.com>
2020-11-15 01:22:06 +0900

    質問コード

--- jaso71788/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ jaso71788/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,6 +1,9 @@
 package com.example.application.views.helloworld;
 
+import com.vaadin.flow.component.HasValue.ValueChangeEvent;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,18 +20,53 @@ import com.vaadin.flow.router.RouteAlias;
 @RouteAlias(value = "", layout = MainView.class)
 public class HelloWorldView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private final TextField name;
+    private final Button sayHello;
+    private final ComboBox<String> combo;
 
     public HelloWorldView() {
         setId("hello-world-view");
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
-        add(name, sayHello);
+        combo = getCmbMode();
+        combo.setItems("item1", "item2", "item3");
+        add(name, sayHello, combo);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
-        sayHello.addClickListener( e-> {
+        sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
+    }
+
+    private ComboBox<String> getCmbMode() {
+        // 略
+        final ComboBox<String> cmbMode = new ComboBox<>();
+
+        cmbMode.addValueChangeListener(new ValueChangeListener<ValueChangeEvent<?>>() {
+            private static final long serialVersionUID = 1L;
+
+            boolean isCalled = false;
+
+            @Override
+            public void valueChanged(final ValueChangeEvent<?> e) {
+                if (isCalled)
+                    return;
+                try {
+                    isCalled = true;
+                    // 確認ダイアログを表示し、
+                    // キャンセルなら
+                    // コンボボックスを変更前の状態にし、リターン
+                    final Object oldValue = e.getOldValue();
+                    getCmbMode().setValue((String) oldValue);
+                    return;
+                    // OKなら
+                    // データをリセットする
+
+                } finally {
+                    isCalled = false;
+                }
+            }
+        });
+        return cmbMode;
     }
 
 }
