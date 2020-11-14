f1047a6ed22d2d977fbf657209928cf1b784d20b
yukihane <yukihane.feather@gmail.com>
2020-11-15 00:35:18 +0900

    https://vaadin.com/start から雛形生成

---
+++ jaso71788/src/main/java/com/example/application/views/about/AboutView.java
@@ -0,0 +1,20 @@
+package com.example.application.views.about;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.example.application.views.main.MainView;
+
+@Route(value = "about", layout = MainView.class)
+@PageTitle("About")
+@CssImport("./styles/views/about/about-view.css")
+public class AboutView extends Div {
+
+    public AboutView() {
+        setId("about-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
