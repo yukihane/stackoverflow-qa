af921fb9b3f2ec91c43cd1e48a54779e5039e6ab
yukihane <yukihane.feather@gmail.com>
2021-01-29 02:56:25 +0900

    回答コード

--- jaso73625/src/main/java/com/example/jaso73625/Books.java
+++ jaso73625/src/main/java/com/example/jaso73625/Books.java
@@ -1,9 +1,9 @@
 package com.example.jaso73625;
 
 import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -15,8 +15,8 @@ public class Books implements Serializable {
     @Id
     private int id;
 
-    @Column(name = "book_category_id")
-    private int bookCategoryId;
+    @ManyToOne
+    private BookCategory bookCategory;
 
     private String name;
 
