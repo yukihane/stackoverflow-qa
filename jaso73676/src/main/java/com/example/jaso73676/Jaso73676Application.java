70ddf6a5bb4fd65b171863477b9225908e8e247d
yukihane <yukihane.feather@gmail.com>
2021-01-30 04:36:38 +0900

    init

---
+++ jaso73676/src/main/java/com/example/jaso73676/Jaso73676Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso73676;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso73676Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso73676Application.class, args);
+	}
+
+}
