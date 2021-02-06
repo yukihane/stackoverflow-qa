c28e92f0daa6c0018f91b621574afb461ce7d72c
yukihane <yukihane.feather@gmail.com>
2021-02-07 06:55:30 +0900

    init

---
+++ so66080463/src/main/java/com/example/so66080463/So66080463Application.java
@@ -0,0 +1,13 @@
+package com.example.so66080463;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So66080463Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So66080463Application.class, args);
+	}
+
+}
---
+++ so66080463/src/test/java/com/example/so66080463/So66080463ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so66080463;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So66080463ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
