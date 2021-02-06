9ec471c17ab102a0579c936e28165c6fe4fac7ef
yukihane <yukihane.feather@gmail.com>
2021-02-06 18:40:38 +0900

    init

---
+++ so66054392/src/test/java/com/example/so66054392/So66054392ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so66054392;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So66054392ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
