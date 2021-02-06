c4c9cf69eeaa880f0295d5229548cafe4740607d
yukihane <yukihane.feather@gmail.com>
2021-02-07 06:46:14 +0900

    回答(constructor injection)

--- so41570821/src/main/java/com/example/so41570821/Config.java
+++ so41570821/src/main/java/com/example/so41570821/Config.java
@@ -1,5 +1,6 @@
 package com.example.so41570821;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +10,13 @@ public class Config {
     private final String q = "AnotherProp";
 
     @Bean
+    @Qualifier("p")
     public String getP() {
         return p;
     }
 
     @Bean
+    @Qualifier("q")
     public String getQ() {
         return q;
     }
--- so41570821/src/main/java/com/example/so41570821/SomeC.java
+++ so41570821/src/main/java/com/example/so41570821/SomeC.java
@@ -1,6 +1,7 @@
 package com.example.so41570821;
 
 import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class SomeC {
 
-    @Resource(name = "getP")
     private String p;
+
+    public SomeC(@Qualifier("p") String p) {
+        this.p = p;
+    }
 
     @GetMapping
     public String index() {
