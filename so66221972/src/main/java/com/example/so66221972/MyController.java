acfc6b0a679e28c9e42fd7ce0b8eb6c0c343c5d8
yukihane <yukihane.feather@gmail.com>
2021-02-17 05:38:27 +0900

    Answer: https://stackoverflow.com/a/66231363/4506703

--- so66221972/src/main/java/com/example/so66221972/MyController.java
+++ so66221972/src/main/java/com/example/so66221972/MyController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @GetMapping("test")
-    public Map<String, Object> index(final UUID uuid) {
+    public Map<String, Object> index(@RequestParam final UUID uuid) {
         return Map.of("uuid", uuid.toString());
     }
 }
