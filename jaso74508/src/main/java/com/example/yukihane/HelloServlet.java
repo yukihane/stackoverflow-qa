445f31898d2cde3d8ea4c384315b7a309587826a
yukihane <yukihane.feather@gmail.com>
2021-03-09 23:03:53 +0900

    https://ja.stackoverflow.com/q/74517/2808 についてのコードも追加

--- jaso74508/src/main/java/com/example/yukihane/HelloServlet.java
+++ jaso74508/src/main/java/com/example/yukihane/HelloServlet.java
@@ -1,6 +1,9 @@
 package com.example.yukihane;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
+        throws ServletException, IOException {
         final String value = System.getProperty("foo");
         System.out.println(value);
+
+        final InputStream res = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.txt");
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(res));
+        final String line = reader.readLine();
+        System.out.println(line);
+
         resp.setStatus(HttpServletResponse.SC_CREATED);
     }
 
