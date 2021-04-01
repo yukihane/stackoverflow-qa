c9a1cd5cced3005ff8ec8303d0b9274a2a6847a2
yukihane <yukihane.feather@gmail.com>
2021-04-02 04:08:19 +0900

    init

---
+++ so66814965/src/main/java/com/example/so66814965/So66814965Application.java
@@ -0,0 +1,13 @@
+package com.example.so66814965;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So66814965Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So66814965Application.class, args);
+	}
+
+}
