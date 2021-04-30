bdad94a70a97e148142659c6288a1c039ee84b13
yukihane <yukihane.feather@gmail.com>
2021-05-01 06:39:29 +0900

    質問コード

--- jaso75594/src/App.jsx
+++ jaso75594/src/App.jsx
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=5&nat=us&inc=gender,name,email`)
+      .then((res) => res.json())
+      .then((json) => setUsers(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table border="1">
+      <tr>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Email</th>
+      </tr>
+      {users.results.map((result) => {
+        return (
+          <tr>
+            <td>{result.name.first}</td>
+            <td>{result.gender}</td>
+            <td>{result.email}</td>
+          </tr>
+        );
+      })}
+    </table>
   );
 }
 
