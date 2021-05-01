15a5449bfb9a77add5387d94d284cd8e103a5288
yukihane <yukihane.feather@gmail.com>
2021-05-01 17:17:23 +0900

    init

---
+++ jaso75599/src/test/java/com/example/jaso75599/Jaso75599ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso75599;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso75599ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
