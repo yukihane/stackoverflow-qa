e9c23f6c45ceac572945e507de512ec1e77127da
yukihane <yukihane.feather@gmail.com>
2021-06-02 06:33:11 +0900

    init

---
+++ jaso76226/src/main/java/com/github/yukihane/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.github.yukihane;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ jaso76226/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.github.yukihane {
+    requires javafx.controls;
+    exports com.github.yukihane;
+}
