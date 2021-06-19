3ff7dc2c4e0cd6ccbbb5a21d154d0a6472c26d3f
yukihane <yukihane.feather@gmail.com>
2021-06-19 20:42:34 +0900

    質問コード

---
+++ jaso77585/src/main/java/com/github/yukihane/TurtleTrap4.java
@@ -0,0 +1,46 @@
+package com.github.yukihane;
+
+public class TurtleTrap4 {
+    Turtle t;
+    int x, y;
+    int dx, dy;
+
+    public void init(int xpos, int ypos, int xspeed, int yspeed) {
+    this.x = xpos;
+    this.y = ypos;
+    this.dx = xspeed;
+    this.dy = yspeed;
+    this.t = new Turtle();
+    this.t.move(xpos, ypos);
+    this.t.penDown();
+    }
+
+    public void setColor(java.awt.Color c)
+
+    public void step() {
+    if (this.x < 20 || 340 < this.x)
+        this.dx = -this.dx;
+    if (this.y < 20 || 340 < this.y)
+        this.dy = -this.dy;
+    
+    this.x += this.dx;
+    this.y += this.dy;
+    this.t.move(this.x, this.y);
+    }
+
+    public static void main(String[] args) {
+    TurtleTrap4 trapA = new TurtleTrap4();
+    trapA.init(100, 200, 10, 5);
+
+    TurtleTrap4 trapB = new TurtleTrap4();
+    trapB.init(150, 250, -10, 3);
+    setColor(RED);
+
+
+
+    while (true) {
+        trapA.step();
+        trapB.step();
+    }
+    }
+}
