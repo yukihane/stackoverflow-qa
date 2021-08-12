a483492254c93b8ec9f8a30ad1819a58f4817ee8
yukihane <yukihane.feather@gmail.com>
2021-08-12 10:57:49 +0900

    コンパイルエラー除去

---
+++ jaso80723/src/main/java/pair_matching/Cardclick_Listener.java
@@ -0,0 +1,42 @@
+package pair_matching;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Cardclick_Listener implements MouseListener {
+
+    public Cardclick_Listener(CardLabel tmp_label) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
