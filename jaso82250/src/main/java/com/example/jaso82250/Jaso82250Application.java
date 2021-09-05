d0ca75d3db6384e5ce9f71f440d37d377b40a1bb
yukihane <yukihane.feather@gmail.com>
2021-09-05 22:11:59 +0900

    init

---
+++ jaso82250/src/main/java/com/example/jaso82250/Jaso82250Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso82250;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso82250Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso82250Application.class, args);
+	}
+
+}
---
+++ jaso82250/src/test/java/com/example/jaso82250/Jaso82250ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso82250;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso82250ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
