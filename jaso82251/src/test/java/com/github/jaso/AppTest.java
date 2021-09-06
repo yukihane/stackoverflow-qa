85284ecba16b5a053146117806c0490fdabc6469
yukihane <yukihane.feather@gmail.com>
2021-09-07 04:43:10 +0900

    init

---
+++ jaso82251/src/test/java/com/github/jaso/AppTest.java
@@ -0,0 +1,19 @@
+package com.github.jaso;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertThat(true).isEqualTo(false);
+    }
+}
