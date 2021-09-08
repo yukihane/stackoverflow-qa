ea4e3e3ee5f275a2d7ad1787f1aab0c5c2528946
yukihane <yukihane.feather@gmail.com>
2021-09-09 01:46:13 +0900

    回答コード

--- jaso82349/src/test/java/com/example/jaso82349/ExampleControllerTest.java
+++ jaso82349/src/test/java/com/example/jaso82349/ExampleControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.jaso82349;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -9,12 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest(webEnvironment  = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 public class ExampleControllerTest {
     @Autowired
@@ -22,21 +24,35 @@ public class ExampleControllerTest {
 
     @Test
     public void postEx1Test() throws Exception {
-        ExampleFormEx1 form = new ExampleFormEx1();
-        form.setFullName("テスト太郎");
-        form.setAge(20);
-        form.setEx1("");
-        String requestForm = new ObjectMapper().writeValueAsString(form);
 
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
-                .post("/")
-                .content(requestForm)
-                .accept("application/json;charset=UTF-8")
-                .contentType("MediaType.APPLICATION_JSON");
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(view().name("example.html"))
-                .andReturn();
+        mockMvc.perform(post("/")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/test/web/servlet/request/MockHttpServletRequestBuilder.html#content-java.lang.String-
+            .content("fullName=%E3%83%86%E3%82%B9%E3%83%88%E5%A4%AA%E9%83%8E&age=20&ex1=")
+            // あるいは param() を使って
+            // .param("fullName", "テスト太郎")
+            // .param("age", "20")
+            // .param("ex1", "")
+            )
+            .andExpect(status().isOk())
+            .andExpect(view().name("example.html"));
+
+        //        
+        //        ExampleFormEx1 form = new ExampleFormEx1();
+        //        form.setFullName("テスト太郎");
+        //        form.setAge(20);
+        //        form.setEx1("");
+        //        String requestForm = new ObjectMapper().writeValueAsString(form);
+        //
+        //        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+        //                .post("/")
+        //                .content(requestForm)
+        //                .accept("application/json;charset=UTF-8")
+        //                .contentType("MediaType.APPLICATION_JSON");
+        //        MvcResult result = mockMvc.perform(request)
+        //                .andExpect(status().isOk())
+        //                .andExpect(view().name("example.html"))
+        //                .andReturn();
 
         // ...テスト処理...
 
@@ -51,14 +67,14 @@ public class ExampleControllerTest {
         String requestForm = new ObjectMapper().writeValueAsString(form);
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
-                .post("/")
-                .content(requestForm)
-                .accept("application/json;charset=UTF-8")
-                .contentType("MediaType.APPLICATION_JSON");
+            .post("/")
+            .content(requestForm)
+            .accept("application/json;charset=UTF-8")
+            .contentType("MediaType.APPLICATION_JSON");
         MvcResult result = mockMvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(view().name("example.html"))
-                .andReturn();
+            .andExpect(status().isOk())
+            .andExpect(view().name("example.html"))
+            .andReturn();
 
         // ...テスト処理...
 
