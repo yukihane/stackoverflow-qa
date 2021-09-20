59219752b154446b2932f567778657f7e165c265
yukihane <yukihane.feather@gmail.com>
2021-09-21 00:02:49 +0900

    質問コード修正

--- jaso82596/src/test/java/com/example/demo/MockMainServiceTest.java
+++ jaso82596/src/test/java/com/example/demo/MockMainServiceTest.java
@@ -4,13 +4,11 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.doReturn;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 //直接依存するクラスをMock化するテスト
@@ -18,16 +16,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MockMainServiceTest {
 
-    @InjectMocks
-    MainServiceImpl mainService;
+    @Autowired
+    MainService mainService;
 
-    @Mock
+    @MockBean
     SubMainService subMainService;
-
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void サブクラスをMock化() {
