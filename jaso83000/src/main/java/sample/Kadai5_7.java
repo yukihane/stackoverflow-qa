a7282077e4b66fbef35bb4fe0fa27f31b66470cd
yukihane <yukihane.feather@gmail.com>
2021-10-11 11:48:06 +0900

    質問コード

---
+++ jaso83000/src/main/java/sample/Kadai5_7.java
@@ -0,0 +1,59 @@
+package sample;
+
+public class Kadai5_7 {
+
+    public static void main(String[] args) {
+        //配列データの宣言・初期化
+        int[] arrays1 = { 96, 45, 31, 29, 84, 77 };
+        //並び替え前の配列内を表示
+        display(arrays1, false);
+        //昇順で並び替えを実施
+        arraysSort(arrays1, true);
+        //並び替え後の配列内を表示
+        display(arrays1, true);
+        //降順で並び替えを実施
+        arraysSort(arrays1, false);
+        //並び替え後の配列内を表示
+        display(arrays1, true);
+    }
+
+    public static void arraysSort(int[] array, boolean orderType) {
+        if (orderType == true) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i; j < array.length; j++) {
+                    if (array[i] < array[j]) {
+                        change(array, i, j);
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i; j < array.length; j++) {
+                    if (array[i] > array[j]) {
+                        change(array, i, j);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void display(int[] array, boolean isSorted) {
+        if (isSorted == true) {
+            System.out.println("****並び替え後****");
+        } else {
+            System.out.println("****並び替え前****");
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + ",");
+            }
+        }
+
+    }
+
+    public static void change(int[] array, int i, int j) {
+        int[] tmp = new int[array.length];
+        tmp[i] = array[i];
+        tmp[j] = array[j];
+        array[i] = tmp[i];
+        array[j] = tmp[j];
+    }
+}
