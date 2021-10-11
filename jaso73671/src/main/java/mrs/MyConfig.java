7d8b437fa55acece2d3e5457854b2b04d88370f7
yukihane <yukihane.feather@gmail.com>
2021-10-11 10:47:59 +0900

    回答

---
+++ jaso73671/src/main/java/mrs/MyConfig.java
@@ -0,0 +1,21 @@
+package mrs;
+
+import java.util.Map;
+import javax.validation.Validator;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+    @Bean
+    public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(final Validator validator) {
+        return new HibernatePropertiesCustomizer() {
+            @Override
+            public void customize(Map<String, Object> hibernateProperties) {
+                hibernateProperties.put("javax.persistence.validation.factory", validator);
+            }
+        };
+    }
+}
