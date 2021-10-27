529079d64f71967da2c603f78ae2e32aa641c24a
yukihane <yukihane.feather@gmail.com>
2021-10-28 06:47:18 +0900

    回答コード

--- so69745344/src/main/java/com/example/so69745344/ItemConfig.java
+++ so69745344/src/main/java/com/example/so69745344/ItemConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class ItemConfig {
 
     @Autowired
