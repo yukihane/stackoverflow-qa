aa75642a35505dd10fca6c347ade890baefb9396
yukihane <yukihane.feather@gmail.com>
2021-11-01 08:42:34 +0900

    問題再現コード

--- so69790410/src/main/java/com/example/so69790410/TblDepartments.java
+++ so69790410/src/main/java/com/example/so69790410/TblDepartments.java
@@ -2,8 +2,10 @@ package com.example.so69790410;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class TblDepartments {
     @Id
     private Integer dbID;
--- so69790410/src/main/java/com/example/so69790410/TblEmployees.java
+++ so69790410/src/main/java/com/example/so69790410/TblEmployees.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class TblEmployees {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
