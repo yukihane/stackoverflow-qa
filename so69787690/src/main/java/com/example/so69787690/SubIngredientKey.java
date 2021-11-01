397b0137da8a1e6e97aae4c99c574c0e7936db32
yukihane <yukihane.feather@gmail.com>
2021-11-01 14:49:19 +0900

    質問コード修正。クラス名が間違っていると思われる。

--- so69787690/src/main/java/com/example/so69787690/SubIngredientKey.java
+++ so69787690/src/main/java/com/example/so69787690/SubIngredientKey.java
@@ -2,10 +2,32 @@ package com.example.so69787690;
 
 import java.io.Serializable;
 import javax.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Embeddable
+@Setter
+@Getter
+@ToString
 public class SubIngredientKey implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String ingredientId;
+    private Long ingredientId;
+
+    private Long subIngredientId;
+    /**
+     * @return the ingredientId
+     */
+    
+     public SubIngredientKey () {}
+     
+     public SubIngredientKey (
+                Long ingredientId,
+                Long subIngredientId) {
+                this.ingredientId = ingredientId;
+                this.subIngredientId = subIngredientId;
+    }
+//omitted code
+
 }
