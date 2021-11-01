908baa1af46ff020990f34f98a2e096f0758ad56
yukihane <yukihane.feather@gmail.com>
2021-11-01 20:36:10 +0900

    再現コード(再現しない)

--- so69796060/src/main/java/com/example/so69796060/So69796060Application.java
+++ so69796060/src/main/java/com/example/so69796060/So69796060Application.java
@@ -1,13 +1,23 @@
 package com.example.so69796060;
 
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class So69796060Application {
+public class So69796060Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(So69796060Application.class, args);
 	}
 
+    @Value("#{new java.text.SimpleDateFormat('dd-MM-yyyy').parse('${param.date}')}")
+    private Date date;
+
+    @Override
+    public void run(final String... args) throws Exception {
+        System.out.println(date);
+    }
 }
--- so69796060/src/test/java/com/example/so69796060/So69796060ApplicationTests.java
+++ so69796060/src/test/java/com/example/so69796060/So69796060ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.so69796060;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(args = "--param.date=01-11-2021")
 class So69796060ApplicationTests {
 
 	@Test
