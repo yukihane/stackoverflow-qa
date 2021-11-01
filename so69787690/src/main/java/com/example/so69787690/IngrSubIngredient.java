66103eff9cbebc966492ca949cc530e1f0546ff2
yukihane <yukihane.feather@gmail.com>
2021-11-01 14:50:35 +0900

    出力用設定

--- so69787690/src/main/java/com/example/so69787690/IngrSubIngredient.java
+++ so69787690/src/main/java/com/example/so69787690/IngrSubIngredient.java
@@ -1,12 +1,17 @@
 package com.example.so69787690;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class IngrSubIngredient {
     
     
@@ -15,10 +20,12 @@ public class IngrSubIngredient {
     
     @ManyToOne(fetch = FetchType.EAGER)
     @MapsId("ingredientId")
+    @JsonBackReference
     private Ingredient ingredient;
     
     @ManyToOne(fetch = FetchType.EAGER)
     @MapsId("subIngredientId")
+    @JsonBackReference
     private Ingredient subIngredient;
     
     private double quantity;
--- so69787690/src/main/java/com/example/so69787690/Ingredient.java
+++ so69787690/src/main/java/com/example/so69787690/Ingredient.java
@@ -1,5 +1,6 @@
 package com.example.so69787690;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
@@ -7,14 +8,19 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Ingredient {
     @Id
     private Long id;
 
 //omitted code
   @OneToMany(mappedBy = "embId.ingredientId", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
     private Set<IngrSubIngredient> ingrSubIngredients =  new HashSet<IngrSubIngredient>();
 //omitted code
 //getters and setters.....
