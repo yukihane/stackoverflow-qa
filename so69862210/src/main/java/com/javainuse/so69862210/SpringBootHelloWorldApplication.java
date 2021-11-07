7cc39f56dc76a6dfbcffe96fc032e7e2d1d41858
yukihane <yukihane.feather@gmail.com>
2021-11-07 14:20:22 +0900

    質問コード

--- so69862210/src/main/java/com/javainuse/so69862210/SpringBootHelloWorldApplication.java
+++ so69862210/src/main/java/com/javainuse/so69862210/SpringBootHelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class So69862210Application {
+public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(So69862210Application.class, args);
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 
 }
