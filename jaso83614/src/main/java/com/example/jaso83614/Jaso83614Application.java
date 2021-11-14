c2747a095cf0d037642251a23056ef3a77fd5dc9
yukihane <yukihane.feather@gmail.com>
2021-11-14 23:04:14 +0900

    実行用spring-bootアプリケーション

---
+++ jaso83614/src/main/java/com/example/jaso83614/Jaso83614Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso83614;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso83614Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso83614Application.class, args);
+	}
+
+}
