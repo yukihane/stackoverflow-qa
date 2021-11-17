2b55cf0f1fd9f00baddfad3152b2c93291fa9b5f
yukihane <yukihane.feather@gmail.com>
2021-11-17 11:12:50 +0900

    整形

--- jaso83660/index.js
+++ jaso83660/index.js
@@ -1,19 +1,37 @@
 TimerCount();
 
-function TimerCount(){
+function TimerCount() {
+  $.get("updatetime.txt", function (data) {
+    const dataArray = data.split(","); // カンマで分割
+    const upDateStart = new Date(
+      parseInt(dataArray[0]),
+      parseInt(dataArray[1]),
+      parseInt(dataArray[2]),
+      parseInt(dataArray[3]),
+      parseInt(dataArray[4]),
+      0,
+      0
+    );
+    const upDateEnd = new Date(
+      parseInt(dataArray[0]),
+      parseInt(dataArray[1]),
+      parseInt(dataArray[2]),
+      parseInt(dataArray[3]),
+      parseInt(dataArray[4]),
+      2,
+      0
+    );
+    const currentDate = new Date(
+      Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000
+    );
 
-  $.get("updatetime.txt", function (data){
+    if (
+      currentDate.getTime() >= upDateStart.getTime() &&
+      currentDate.getTime() <= upDateEnd.getTime()
+    ) {
+      window.location.reload();
+    }
+  });
 
-            const dataArray = data.split(',');  // カンマで分割
-            const upDateStart = new Date(parseInt(dataArray[0]), parseInt(dataArray[1]), parseInt(dataArray[2]), parseInt(dataArray[3]), parseInt(dataArray[4]),0, 0);
-            const upDateEnd = new Date(parseInt(dataArray[0]), parseInt(dataArray[1]), parseInt(dataArray[2]), parseInt(dataArray[3]), parseInt(dataArray[4]),2, 0);
-            const currentDate= new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
-
-            if(currentDate.getTime() >= upDateStart.getTime() && currentDate.getTime() <= upDateEnd.getTime()) {
-              window.location.reload();
-            } 
-        }
-  );
-
-    setTimeout("TimerCount()",1000);
+  setTimeout("TimerCount()", 1000);
 }
