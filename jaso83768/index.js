d77fc7633d4715c8b7bcefe5cc7c561d2c70bd56
yukihane <yukihane.feather@gmail.com>
2021-11-23 15:31:51 +0900

    回答コード

--- jaso83768/index.js
+++ jaso83768/index.js
@@ -1,7 +1,9 @@
-function controlselect() {
-  let elements = document.getElementsByClassName("disselect");
+// 引数で要素のグループを指定する
+function controlselect(target) {
+  let elements = target.getElementsByClassName("disselect");
   let len = elements.length;
-  var sele_val = document.getElementById("other").value;
+  // id は使えないので class に変更
+  var sele_val = target.getElementsByClassName("other")[0].value;
   if (sele_val === "その他") {
     for (let i = 0; i < len; i++) {
       elements.item(i).removeAttribute("disabled");
@@ -13,4 +15,13 @@ function controlselect() {
   }
 }
 
-controlselect();
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = ["elements1", "elements2", "elements3"];
+  for (let t of targets) {
+    const div = document.getElementById(t);
+    div
+      .getElementsByClassName("other")[0]
+      .addEventListener("change", () => controlselect(div));
+    controlselect(div);
+  }
+});
