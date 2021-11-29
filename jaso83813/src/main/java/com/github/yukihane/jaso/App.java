53234ad1e964724605531727ea9808e47c73fa0b
yukihane <yukihane.feather@gmail.com>
2021-11-30 00:10:16 +0900

    init

---
+++ jaso83813/src/main/java/com/github/yukihane/jaso/App.java
@@ -0,0 +1,11 @@
+package com.github.yukihane.jaso;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
