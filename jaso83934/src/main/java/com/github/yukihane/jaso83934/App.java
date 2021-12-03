ab7afc88c5a77c183c9ffa3989e6ee244dcd055f
yukihane <yukihane.feather@gmail.com>
2021-12-03 22:31:06 +0900

    回答コード

--- jaso83934/src/main/java/com/github/yukihane/jaso83934/App.java
+++ jaso83934/src/main/java/com/github/yukihane/jaso83934/App.java
@@ -1,11 +1,16 @@
 package com.github.yukihane.jaso83934;
 
-/**
- * Hello world!
- */
-public class App {
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
+public class App {
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        final LocalDate base = LocalDate.now(); // 2021-12-03
+
+        // 当月(2021-12) の 2 回目の水曜日
+        final LocalDate date = base.with(TemporalAdjusters.dayOfWeekInMonth(2, DayOfWeek.WEDNESDAY));
+
+        System.out.println(date); // 2021-12-08
     }
 }
