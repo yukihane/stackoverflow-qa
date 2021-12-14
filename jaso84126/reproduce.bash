cbdb77f04ce48792c1b366bb99a6d73ff1af6ea2
yukihane <yukihane.feather@gmail.com>
2021-12-14 22:18:53 +0900

    再現手順が分かったのでスクリプト化

---
+++ jaso84126/reproduce.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir workrepo
+cd workrepo
+git init -b main
+git commit --allow-empty -m init
+echo '* text eol=crlf' > .gitattributes
+git add .gitattributes && git commit -m 'add .gitattributes'
+git checkout -b feature
+git rm .gitattributes && git commit -m 'remove .gitattributes'
+echo -e 'hello\nworld' | unix2dos > hello.txt
+git add hello.txt && git commit -m 'add hello.txt'
+GIT_SEQUENCE_EDITOR="sed -i '1d'" git rebase -i main
+git checkout -f main
+git merge --no-ff feature
