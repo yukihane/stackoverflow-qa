ee1e5d66755469a371b9b409e075291dc09636c2
yukihane <yukihane.feather@gmail.com>
2021-12-15 04:21:44 +0900

    実行時の問題を改善

--- jaso84126/reproduce.bash
+++ jaso84126/reproduce.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux
+
 mkdir workrepo
 cd workrepo
 git init -b main
@@ -9,7 +11,7 @@ git add .gitattributes && git commit -m 'add .gitattributes'
 git checkout -b feature
 git rm .gitattributes && git commit -m 'remove .gitattributes'
 echo -e 'hello\nworld' | unix2dos > hello.txt
-git add hello.txt && git commit -m 'add hello.txt'
+git add hello.txt && git commit -m 'add hello.txt with CRLF'
 GIT_SEQUENCE_EDITOR="sed -i '1d'" git rebase -i main
 git checkout -f main
-git merge --no-ff feature
+git merge --no-ff --no-edit feature
