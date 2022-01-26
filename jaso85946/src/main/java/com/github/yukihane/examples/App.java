2a51b436e6c0c42478e60bec42fd7a050c1b3680
yukihane <yukihane.feather@gmail.com>
2022-01-27 05:09:54 +0900

    回答コード

--- jaso85946/src/main/java/com/github/yukihane/examples/App.java
+++ jaso85946/src/main/java/com/github/yukihane/examples/App.java
@@ -1,11 +1,20 @@
 package com.github.yukihane.examples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        final List<String> str = new ArrayList<>();
+        str.add("123abc");
+        str.add("456[def]");
+
+        System.out.println(str); // [123abc]
+        System.out.println(str.toString().replaceAll("^\\[|\\]$", "")); // 123abc
+        System.out.println(String.join(", ", str));
     }
 }
