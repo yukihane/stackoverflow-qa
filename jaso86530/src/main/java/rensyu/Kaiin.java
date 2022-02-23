4381aafb12143068bfe37675e6860351c855ab71
yukihane <yukihane.feather@gmail.com>
2022-02-24 08:30:31 +0900

    質問コード

---
+++ jaso86530/src/main/java/rensyu/Kaiin.java
@@ -0,0 +1,25 @@
+package rensyu;
+import java.util.Date;
+public class Kaiin {
+    int id;
+    String name;
+    Date addDate;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getAddDate() {
+        return addDate;
+    }
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+}
---
+++ jaso86530/src/main/java/rensyu/Register.java
@@ -0,0 +1,38 @@
+package rensyu;
+
+import java.util.Date;
+import java.util.*;
+
+public class Register {
+    static ArrayList<Kaiin> kaiinAll = new ArrayList<Kaiin>();
+
+    public static void main(String[] args) {
+        Register r = new Register();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("何人登録しますか？");
+        int i = scan.nextInt();
+        int id = 0;
+        String name = null;
+        for (int j = 0; j < i; j++) {
+            System.out.println("会員IDを入力してください");
+            id = scan.nextInt();
+            System.out.println("名前を入力してください");
+            name = scan.next();
+            kaiinAll.add(r.kaiinAdd(id, name));
+            System.out.println(kaiinAll.get(id));
+            
+        }
+
+    }
+
+    private Kaiin kaiinAdd(int id, String name) {
+        Kaiin k = new Kaiin();
+        k.setId(id);
+        k.setName(name);
+        Date d = new Date();
+        k.setAddDate(d);
+        return k;
+
+    }
+
+}
