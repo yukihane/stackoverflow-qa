b4334e44db1fd00a70f2ae90a0869a1e7cd3401a
yukihane <yukihane.feather@gmail.com>
2022-03-23 18:25:08 +0900

    回答コード

--- jaso87971/src/question.exs
+++ jaso87971/src/question.exs
@@ -1,19 +1,15 @@
 defmodule Test do
-  def func(input1, input2) do
-    funcInside(input1, input2)
-  end
+  def func([], []), do: []
+  def func([h1 | t1], [h2 | t2]), do: [func(h1, h2) | func(t1, t2)]
+  def func(input1, input2), do: input1 * input2
+end
 
-  # まず単純に第1引数と第2引数を掛け算して出力する
-  defp funcInside(input1, input2) do
-    item1 = hd input1
-    item2 = hd input2
-    result = item1 * item2
-    IO.puts "#{outputList}" #　←　出力をどう書けばコンソールにリストのまま表示されるかわからない。
-  end
+# 例１：
+ex1 = Test.func([1], [2])
+# => [2] ←コンソールに上記プログラムで処理した後のリストを表示したいです。
+IO.inspect(ex1, charlists: :as_lists)
 
-#例１：
-Test.func([1], [2])
-#=> [2] ←コンソールに上記プログラムで処理した後のリストを表示したいです。
-#例2：
-Test.func([[1, 2, [[2]]]], [[3, 2, [[4]]]])
-#=> [[3, 4, [[8]]]] ←コンソールに上記プログラムで処理した後のリストを表示したいです。
+# 例2：
+ex2 = Test.func([[1, 2, [[2]]]], [[3, 2, [[4]]]])
+# => [[3, 4, [[8]]]] ←コンソールに上記プログラムで処理した後のリストを表示したいです。
+IO.inspect(ex2, charlists: :as_lists)
