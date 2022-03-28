da3658a28d59a8e29583d9b3f9c1495d88f3add3
yukihane <yukihane.feather@gmail.com>
2022-03-29 04:31:35 +0900

    回答コード

--- jaso88076/src/main/java/com/github/yukihane/examples/Main.java
+++ jaso88076/src/main/java/com/github/yukihane/examples/Main.java
@@ -1,19 +1,23 @@
 package com.github.yukihane.examples;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        try {
-            System.out.print("年齢を打ってください:");
-            int age = sc.nextInt();
+        while (true) {
+            try {
+                System.out.print("年齢を打ってください:");
+                int age = sc.nextInt();
 
-            System.out.println("あなたは " + age + "歳です。!");
-        } catch (Exception e) {
-            System.out.println("あなたの年齢を打ってください。");
+                System.out.println("あなたは " + age + "歳です。!");
+                break;
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("あなたの年齢を打ってください。");
+            }
         }
-
     }
 }
