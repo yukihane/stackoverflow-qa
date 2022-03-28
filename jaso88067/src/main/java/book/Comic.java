16b5342d7b28371e317e93d17e4650095a1c698d
yukihane <yukihane.feather@gmail.com>
2022-03-29 05:15:46 +0900

    質問コード

---
+++ jaso88067/src/main/java/book/Comic.java
@@ -0,0 +1,14 @@
+package book;
+
+public class Comic {
+
+    private final String series;
+    private final int volume;
+
+    public Comic(String series, int volume) {
+        this.series = series;
+        this.volume = volume;
+    }
+
+    // ...
+}
---
+++ jaso88067/src/main/java/book/ShortComic.java
@@ -0,0 +1,11 @@
+package book;
+
+import book.Comic;
+
+public class ShortComic extends Comic {
+
+    public void Comic(String series) {
+        super();
+        this.volume = 1;
+    }
+}
