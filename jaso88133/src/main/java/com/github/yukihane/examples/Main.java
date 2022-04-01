c58f2b560d32f2e443d7dbc3235c3dc2b42a7207
yukihane <yukihane.feather@gmail.com>
2022-04-02 02:42:28 +0900

    コメント

--- jaso88133/src/main/java/com/github/yukihane/examples/Main.java
+++ jaso88133/src/main/java/com/github/yukihane/examples/Main.java
@@ -6,10 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
+        // 要素数が5の配列 nums を宣言して初期値を代入
         int[] nums = { 1, 2, 3, 4, 5 };
+        // num を宣言して、 配列 nums の要素(nums[0], nums[1], ..., nums[4])のうちのひとつを代入
         int num = nums[random.nextInt(5)];
 
+        // 3回繰り返す
         for (int i = 0; i < 3; i++) {
+            // num を出力する
             System.out.println(num);
         }
 
