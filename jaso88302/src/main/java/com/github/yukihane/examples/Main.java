054811ca6ce0db03e02e4f647f486797f204a177
yukihane <yukihane.feather@gmail.com>
2022-04-13 06:29:58 +0900

    回答コード

--- jaso88302/src/main/java/com/github/yukihane/examples/Main.java
+++ jaso88302/src/main/java/com/github/yukihane/examples/Main.java
@@ -1,26 +1,29 @@
 package com.github.yukihane.examples;
 
-import java.awt.*;
 import javax.swing.*;
-public class Main{
-    public static void main (String[] args){
-        JFrame frame = new JFrame("GUI");
-        int w = 600, h = 400;
+import java.awt.*;
 
-        frame.setSize(w,h);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("GUI");
+            int w = 600, h = 400;
 
-        JLabel label = new JLabel("Hello World!");
-        frame.getContentPane().setLayout(new FlowLayout());
-        frame.getContentPane().add(label);
+            frame.setSize(w, h);
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton button = new JButton("ここのボタンを押したらHello World!と出力したい");
-        frame.getContentPane().add(button);
+            JLabel label = new JLabel("Hello World!");
+            label.setVisible(false);
+            frame.getContentPane().setLayout(new FlowLayout());
+            frame.getContentPane().add(label);
 
-
-
-
+            JButton button = new JButton("ここのボタンを押したらHello World!と出力したい");
+            button.addActionListener(e -> {
+                label.setVisible(true);
+            });
+            frame.getContentPane().add(button);
+        });
     }
 }
