6c44363aae388f6ab59135ad3616dc2d74411363
yukihane <yukihane.feather@gmail.com>
2022-04-14 07:34:19 +0900

    質問コード

---
+++ jaso88324/src/main/java/com/github/yukihane/examples/Main.java
@@ -0,0 +1,20 @@
+package com.github.yukihane.examples;
+
+import java.util.ArrayList;
+import java.util.Random;
+public class Main{
+    public static void main (String[] args){
+
+        Random random = new Random();
+        ArrayList<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Ruby");
+        languages.add("PHP");
+        languages.add("Swift");
+        languages.add("Python");
+
+        String language = languages[random.nextInt(5)];
+        System.out.println(language);
+
+    }
+}
