f5e95938135d86cfd9922b2db78f15980d809e7e
yukihane <yukihane.feather@gmail.com>
2022-04-21 02:29:25 +0900

    質問コード？

---
+++ jaso88397/src/main/java/com/github/yukihane/examples/Person.java
@@ -0,0 +1,15 @@
+package com.github.yukihane.examples;
+
+public class Person {
+
+  private String name;
+  private int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+}
