58058f262afe76b954be28a2772185d69b03fd41
yukihane <yukihane.feather@gmail.com>
2022-05-14 23:51:24 +0900

    init

---
+++ jaso88808/src/main/kotlin/com/github/yukihane/examples/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.yukihane.examples
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ jaso88808/src/test/kotlin/com/github/yukihane/examples/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.yukihane.examples
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
