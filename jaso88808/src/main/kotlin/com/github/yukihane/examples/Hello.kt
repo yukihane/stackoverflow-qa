8fa1b4e11c0d8fc3efbb4d809c7dc176f3c19d91
yukihane <yukihane.feather@gmail.com>
2022-05-15 01:59:28 +0900

    回答コード

--- jaso88808/src/main/kotlin/com/github/yukihane/examples/Hello.kt
+++ jaso88808/src/main/kotlin/com/github/yukihane/examples/Hello.kt
@@ -1,6 +1,23 @@
 package com.github.yukihane.examples
 
-fun main(args: Array<String>) {
-    println("Hello, World")
+import java.util.function.Function
+
+fun main() {
+    val p1 = Person("タケシ", 5)
+    val p2 = Person("ケンタ", 5)
+    val p3 = Person("ユミ", 8)
+    val list = listOf(p1, p2, p3)
+
+    val isAge5 = fun(p: Person): Boolean =
+        p.age == 5
+
+    val list_filtered = list.filter(isAge5)
+    list_filtered.forEach { println("${it}は5歳です") }
 }
+
+fun isAge5(p: Person): Boolean {
+    return p.age == 5
+}
+
+data class Person(val name: String, val age: Int)
 
