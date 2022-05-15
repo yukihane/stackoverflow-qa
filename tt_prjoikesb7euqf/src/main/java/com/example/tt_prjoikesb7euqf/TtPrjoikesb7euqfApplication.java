cf0c915ddc7b86132c42ce5497bd6814431b1e34
yukihane <yukihane.feather@gmail.com>
2022-05-16 03:17:04 +0900

    init

---
+++ tt_prjoikesb7euqf/src/main/java/com/example/tt_prjoikesb7euqf/TtPrjoikesb7euqfApplication.java
@@ -0,0 +1,13 @@
+package com.example.tt_prjoikesb7euqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtPrjoikesb7euqfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtPrjoikesb7euqfApplication.class, args);
+	}
+
+}
---
+++ tt_prjoikesb7euqf/src/test/java/com/example/tt_prjoikesb7euqf/TtPrjoikesb7euqfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tt_prjoikesb7euqf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtPrjoikesb7euqfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
