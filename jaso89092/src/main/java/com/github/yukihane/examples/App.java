52bc4369e84bec1931680a17f64492e65704af0d
yukihane <yukihane.feather@gmail.com>
2022-05-28 06:40:06 +0900

    snippet

--- jaso89092/src/main/java/com/github/yukihane/examples/App.java
+++ jaso89092/src/main/java/com/github/yukihane/examples/App.java
@@ -3,13 +3,8 @@ package com.github.yukihane.examples;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.graphics.FontMetrics;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -21,30 +16,24 @@ public class App {
     public static void main(final String[] args) {
         final Display display = new Display();
         final Shell shell = new Shell(display);
+        shell.setText("Snippet 100");
         shell.setBounds(10, 10, 200, 200);
-
-        final Label label = new Label(shell, SWT.NONE | SWT.BORDER);
-        //        final Font font = new Font(Display.getDefault(), "Arial", 8, SWT.NONE);
-        final FontData[] fontData = label.getFont().getFontData();
-        for (final FontData fd : fontData) {
-            fd.setName("Arial");
-            fd.setHeight(8);
-            fd.setStyle(SWT.NONE);
+        final Text text = new Text(shell, SWT.MULTI);
+        final Rectangle clientArea = shell.getClientArea();
+        text.setBounds(clientArea.x + 10, clientArea.y + 10, 150, 150);
+        final Font initialFont = text.getFont();
+        final FontData[] fontData = initialFont.getFontData();
+        for (final FontData element : fontData) {
+            element.setHeight(24);
         }
-        final Font font = new Font(Display.getDefault(), fontData);
-        label.setFont(font);
-        label.setText("Hello, world! Hello, world! Hello, world!");
-
-        final FormLayout layout = new FormLayout();
-        layout.marginWidth = layout.marginHeight = 5;
-        shell.setLayout(layout);
-
+        final Font newFont = new Font(display, fontData);
+        text.setFont(newFont);
         shell.open();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch())
                 display.sleep();
         }
-        font.dispose();
+        newFont.dispose();
         display.dispose();
     }
 }
