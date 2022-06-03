daafede97b9b033e030371e646cdf4f9e76d2e08
yukihane <yukihane.feather@gmail.com>
2022-06-04 00:28:21 +0900

    init

---
+++ tt_dq1kc7wigzytdm/src/main/java/com/example/Dq1kc7wigzytdmApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dq1kc7wigzytdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dq1kc7wigzytdmApplication.class, args);
+	}
+
+}
