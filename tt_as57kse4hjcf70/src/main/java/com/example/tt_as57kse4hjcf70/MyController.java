c7e85e25fd78b059ec319722a4ef1603399d0453
yukihane <yukihane.feather@gmail.com>
2022-06-05 05:16:08 +0900

    質問コード

---
+++ tt_as57kse4hjcf70/src/main/java/com/example/tt_as57kse4hjcf70/MyController.java
@@ -0,0 +1,41 @@
+package com.example.tt_as57kse4hjcf70;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+
+    @Data
+    public static class Form {
+        @NotBlank(message = "名前を入力してください")
+        public String inputName;
+    }
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping
+    public String post(@RequestParam("input") final String input,
+        @Validated final Form Form,
+        final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            //エラーの場合の処理
+            // 　　　※②System.out.println(Form);
+            return "errorpage";
+        } else {
+            // 　　　//正常時の処理
+            return "okpage";
+        }
+    }
+
+}
