8eded7fca2690920aa033892d7293e5e73cc7e32
yukihane <yukihane.feather@gmail.com>
2022-06-05 05:05:13 +0900

    init

---
+++ tt_as57kse4hjcf70/src/main/java/com/example/tt_as57kse4hjcf70/TtAs57kse4hjcf70Application.java
@@ -0,0 +1,13 @@
+package com.example.tt_as57kse4hjcf70;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtAs57kse4hjcf70Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtAs57kse4hjcf70Application.class, args);
+	}
+
+}
---
+++ tt_as57kse4hjcf70/src/test/java/com/example/tt_as57kse4hjcf70/TtAs57kse4hjcf70ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tt_as57kse4hjcf70;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtAs57kse4hjcf70ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
