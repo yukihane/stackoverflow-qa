23e923b7706f82fb8eb17d7dc4dffdacc4a0cb94
yukihane <yukihane.feather@gmail.com>
2022-06-09 05:34:16 +0900

    mock server init

---
+++ jaso89284/mock-server/src/main/java/com/example/mockserver/MockServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerApplication.class, args);
+	}
+
+}
