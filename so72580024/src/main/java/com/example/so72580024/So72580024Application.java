241663cba313406edf4d4c11d8fa405a3a99d0e7
yukihane <yukihane.feather@gmail.com>
2022-06-11 09:50:05 +0900

    init

---
+++ so72580024/src/main/java/com/example/so72580024/So72580024Application.java
@@ -0,0 +1,13 @@
+package com.example.so72580024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So72580024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So72580024Application.class, args);
+	}
+
+}
---
+++ so72580024/src/test/java/com/example/so72580024/So72580024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.so72580024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So72580024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
