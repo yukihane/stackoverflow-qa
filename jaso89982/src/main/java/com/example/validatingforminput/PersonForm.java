166e8845f4771d3e85b1ecfb08aae1ebe9ca7a7c
yukihane <yukihane.feather@gmail.com>
2022-07-15 04:53:11 +0900

    質問コード

--- jaso89982/src/main/java/com/example/validatingforminput/PersonForm.java
+++ jaso89982/src/main/java/com/example/validatingforminput/PersonForm.java
@@ -1,37 +1,17 @@
 package com.example.validatingforminput;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class PersonForm {
 
-    @NotNull
-    @Size(min = 2, max = 30)
+    @NotEmpty
     private String name;
 
     @NotNull
     @Min(18)
     private Integer age;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(final Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person(Name: " + this.name + ", Age: " + this.age + ")";
-    }
 }
