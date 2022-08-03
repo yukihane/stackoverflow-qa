89199422635476940bce36bdf75361e6f95bad86
yukihane <yukihane.feather@gmail.com>
2022-08-03 09:55:27 +0900

    回答

--- jaso90301/src/main/java/com/github/yukihane/examples/App.java
+++ jaso90301/src/main/java/com/github/yukihane/examples/App.java
@@ -1,11 +1,30 @@
 package com.github.yukihane.examples;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        String resource = """
+                1.ああああああああああああ
+                2.いいいいいいいいいいいいい
+                3.うううううううううううううううう
+                ええええええええ6.jpgええええええ
+                おおおおおおおおおおおおおお
+                4.あああああああああ
+                ああああああああああああああ
+                あああああ6.70.ああああ
+                """;
+
+        Pattern pattern = Pattern.compile("(?:^|\\n)(\\d+\\..*?)(?=\\n\\d+\\.|\\n$)", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(resource);
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+            System.out.println();
+        }
     }
 }
