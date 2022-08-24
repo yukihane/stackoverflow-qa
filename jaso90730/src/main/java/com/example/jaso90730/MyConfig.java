1caa81e07943cbf0d4555d382f89c1aeda8b3745
yukihane <yukihane.feather@gmail.com>
2022-08-24 20:28:13 +0900

    回答

--- jaso90730/src/main/java/com/example/jaso90730/MyConfig.java
+++ jaso90730/src/main/java/com/example/jaso90730/MyConfig.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 // https://www.baeldung.com/spring-boot-configure-multiple-datasources
 @Configuration
@@ -46,6 +49,18 @@ public class MyConfig {
     @Bean
     public JdbcTemplate oracleJdbcTemplate(@Qualifier("oracleDataSource") final DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSourceInitializer mysqlDataSourceInitializer(@Qualifier("mysqlDataSource") final DataSource datasource) {
+        final ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("schema.sql"));
+        resourceDatabasePopulator.addScript(new ClassPathResource("data.sql"));
+
+        final DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(datasource);
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
     }
 
 }
