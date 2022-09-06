e47bc2ef1aa2f1fb759ee2acf0a9583669161632
yukihane <yukihane.feather@gmail.com>
2022-09-06 21:33:00 +0900

    init

---
+++ jaso90958/src/main/kotlin/com/example/jaso90958/Jaso90958Application.kt
@@ -0,0 +1,11 @@
+package com.example.jaso90958
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Jaso90958Application
+
+fun main(args: Array<String>) {
+	runApplication<Jaso90958Application>(*args)
+}
---
+++ jaso90958/src/test/kotlin/com/example/jaso90958/Jaso90958ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jaso90958
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Jaso90958ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
