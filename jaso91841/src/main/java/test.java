a1394837452e5487321dc824e65ccff871d42cb5
yukihane <yukihane.feather@gmail.com>
2022-10-27 21:56:29 +0900

    回答コード

--- jaso91841/src/main/java/test.java
+++ jaso91841/src/main/java/test.java
@@ -13,28 +13,7 @@ class test {
             BufferedReader reader = new BufferedReader(new FileReader("txt1.txt"));
             BufferedReader reader1 = new BufferedReader(new FileReader("txt2.txt"));
             BufferedReader reader2 = new BufferedReader(new FileReader("txt3.txt"));
-
-            String line;
-
-            while (null != (line = reader.readLine())) {
-                System.out.println(line);
-            }
-
-            String line1;
-            while (null != (line1 = reader1.readLine())) {
-                System.out.println(line1);
-            }
-
-            String line2;
-
-            while (null != (line2 = reader2.readLine())) {
-                System.out.println(line2);
-            }
-
-            reader.close();
-            reader1.close();
-            reader2.close();
-
+            
             File file = new File("txt4.txt");
             File file1 = new File("txt5.txt");
             File file2 = new File("txt6.txt");
@@ -45,9 +24,32 @@ class test {
             PrintWriter writer1 = new PrintWriter(new BufferedWriter(new FileWriter(file1)));
             PrintWriter writer2 = new PrintWriter(new BufferedWriter(new FileWriter(file2)));
 
-            writer.println(line);
-            writer1.println(line1);
-            writer2.println(line2);
+
+            String line;
+
+            while (null != (line = reader.readLine())) {
+                System.out.println(line);
+                writer.println(line);
+            }
+
+            String line1;
+            while (null != (line1 = reader1.readLine())) {
+                System.out.println(line1);
+                writer1.println(line1);
+            }
+
+            String line2;
+
+            while (null != (line2 = reader2.readLine())) {
+                System.out.println(line2);
+                writer2.println(line2);
+            }
+
+            reader.close();
+            reader1.close();
+            reader2.close();
+
+
 
             writer.close();
             writer1.close();
