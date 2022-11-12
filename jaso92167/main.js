8262f18c8fd055db46b9e36af25bee2c6569b2e7
yukihane <yukihane.feather@gmail.com>
2022-11-12 16:08:28 +0900

    整形

--- jaso92167/main.js
+++ jaso92167/main.js
@@ -1,27 +1,24 @@
-let l_stones: number[] = []
-let index = 0
-let A0: Position = null
+let l_stones: number[] = [];
+let index = 0;
+let A0: Position = null;
 player.onChat("comehere", function () {
-    agent.teleportToPlayer()
-})
+  agent.teleportToPlayer();
+});
 player.onChat("1", function () {
-    for (let index2 = 0; index2 < 4; index2++) {
-        _1()
+  for (let index2 = 0; index2 < 4; index2++) {
+    _1();
+  }
+});
+function _1() {
+  l_stones = [STONE, GRASS, WOOL];
+  for (let index2 = 0; index2 < 5; index2++) {
+    index = 0;
+    A0 = agent.getPosition();
+    for (let index2 = 0; index2 < l_stones.length; index2++) {
+      blocks.place(l_stones[index], positions.add(A0, pos(0, index, 0)));
+      index += 1;
     }
-})
-function _1 () {
-    l_stones = [STONE, GRASS, WOOL]
-    for (let index2 = 0; index2 < 5; index2++) {
-        index = 0
-        A0 = agent.getPosition()
-        for (let index2 = 0; index2 < l_stones.length; index2++) {
-            blocks.place(l_stones[index], positions.add(
-            A0,
-            pos(0, index, 0)
-            ))
-            index += 1
-        }
-        agent.move(FORWARD, 2)
-    }
-    agent.turn(RIGHT_TURN)
+    agent.move(FORWARD, 2);
+  }
+  agent.turn(RIGHT_TURN);
 }
