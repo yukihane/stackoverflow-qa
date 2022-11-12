030ce0025c190fcf4aa0645c714b0e479209c0f2
yukihane <yukihane.feather@gmail.com>
2022-11-13 08:35:49 +0900

    回答コード

--- jaso92167/main.js
+++ jaso92167/main.js
@@ -1,24 +1,27 @@
-let l_stones: number[] = [];
-let index = 0;
-let A0: Position = null;
+let l_stones = [STONE, GRASS, WOOL];
+
 player.onChat("comehere", function () {
   agent.teleportToPlayer();
 });
+
 player.onChat("1", function () {
-  for (let index2 = 0; index2 < 4; index2++) {
+  // 4辺分繰り返す
+  for (let side = 0; side < 4; side++) {
     _1();
   }
 });
+
 function _1() {
-  l_stones = [STONE, GRASS, WOOL];
-  for (let index2 = 0; index2 < 5; index2++) {
-    index = 0;
-    A0 = agent.getPosition();
-    for (let index2 = 0; index2 < l_stones.length; index2++) {
-      blocks.place(l_stones[index], positions.add(A0, pos(0, index, 0)));
-      index += 1;
-    }
+  // 4柱分繰り返す
+  for (let pole = 0; pole < 4; pole++) {
+    // 2歩進む
     agent.move(FORWARD, 2);
+    let A0 = agent.getPosition();
+    // 3ブロック(l_stones.length)積む
+    for (let i = 0; i < l_stones.length; i++) {
+      blocks.place(l_stones[i], positions.add(A0, pos(0, i + 1, 0)));
+    }
   }
+  // 右を向く
   agent.turn(RIGHT_TURN);
 }
