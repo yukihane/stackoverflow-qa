f99f8153dcc8fc41662409abac93307209ee70ec
yukihane <yukihane.feather@gmail.com>
2022-12-23 20:28:52 +0900

    init

---
+++ jaso92970/src/main/java/com/example/jaso92970/Jaso92970Application.java
@@ -0,0 +1,13 @@
+package com.example.jaso92970;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaso92970Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaso92970Application.class, args);
+	}
+
+}
---
+++ jaso92970/src/test/java/com/example/jaso92970/Jaso92970ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso92970;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso92970ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
