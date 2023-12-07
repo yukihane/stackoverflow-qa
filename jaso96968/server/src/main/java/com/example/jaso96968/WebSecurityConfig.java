a20c05f352a83ffbcd2b3f4d1fa53119ef7108d3
yukihane <yukihane.feather@gmail.com>
2023-12-07 11:21:44 +0900

    spring securityのCORS設定

---
+++ jaso96968/server/src/main/java/com/example/jaso96968/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.jaso96968;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+        return http.build();
+    }
+}
