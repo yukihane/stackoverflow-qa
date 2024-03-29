919737d1ff450e2110101b150926cf919d1f36e8
yukihane <yukihane.feather@gmail.com>
2024-03-29 22:01:04 +0900

    init

---
+++ jaso98990/src/test/java/com/example/jaso98990/Jaso98990ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaso98990;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaso98990ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
