66314beaab018b589c8f4889cb89fb8f33d0f5d7
yukihane <yukihane.feather@gmail.com>
2025-01-11 02:36:16 +0900

    回答

--- jaso100624/vite.config.ts
+++ jaso100624/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        buildMode: true,
+      },
+    }),
+  ],
+});
